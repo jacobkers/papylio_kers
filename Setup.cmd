0efe3d1c6ae2342e4706dcc8c18152840433e74b
Ivo Severins <i.w.h.severins@tudelft.nl>
2019-08-22 09:28:31 +0200

    Update Setup.cmd

--- Setup.cmd
+++ Setup.cmd
@@ -3,7 +3,7 @@ ECHO  %~dp0%.git\
 IF EXIST %~dp0%.git\ (
 	ECHO 'Already a git repository present'
 ) ELSE (
-	git clone -b develop --depth=1 https://gitlab.tudelft.nl/ivoseverins/traceanalysis.git
+	git clone --depth=1 https://gitlab.tudelft.nl/ivoseverins/traceanalysis.git
 	CALL traceanalysis\Libraries.cmd
 )
 pause
