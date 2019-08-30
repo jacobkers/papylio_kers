49ab11f892c2e90b98e8a7c3f8f525bf2eebd25f
Ivo Severins <i.w.h.severins@tudelft.nl>
2019-08-30 14:32:28 +0200

    Changed setup, libraries and readme to conda environment

--- Libraries.cmd
+++ Libraries.cmd
@@ -1,4 +1,4 @@
-py -3.7 -m pip install numpy==1.16.4
-py -3.7 -m pip install matplotlib==3.1.0
-py -3.7 -m pip install wxpython==4.0.4
-py -3.7 -m pip install pathlib2==2.3.4
+conda install -y -c conda-forge numpy=1.17.1
+conda install -y -c conda-forge matplotlib=3.1.1
+conda install -y -c conda-forge wxpython=4.0.6
+conda install -y -c conda-forge pathlib2=2.3.4
