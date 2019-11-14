Correct time calculation in file class

dwellAnalysis.py
@@ -14,9 +14,9 @@ import seaborn as sns
 sns.set(style="ticks")
 sns.set_color_codes()
 
-import traceAnalysisCode as analysis
+from trace_analysis import Experiment
 
-def get_dwell_hist(dwells, dwelltype='offtime', nbins=20, save=True, plot=True,
+def get_dwell_hist(dwells, dwelltype='offtime', nbins=10, save=True, plot=True,
                    extra_label='', log=False):
 
     #  select only the ones that don't exceed the total measurement time minus 10 sec
@@ -59,23 +59,39 @@ def get_average_dwell(dwells):
     return avrg_dwell
 
 if __name__ == '__main__':
-    filename = 'O:/SM-data/20191024_dcas9_DNA05-06-09-Cy3/'
-    chamber = '#4.20_streptadivin_0.5nM_dcas9-crRNA-Cy5_10nM_DNA05-Cy3_G_movies_lowsalt'
-    filename += chamber + '/'
-    dwells_all = []
-    for path in os.listdir(filename):
-        if 'dwells_data' in path:
+    filename = 'G:/SM-data/20191101_dcas9_flow_DNA04_DNA20/'
+    chamber = '#3.20_streptavidin_0.5nM_dcas9-crRNA-Cy5_10nM_DNA20-Cy3_G_flow'
+    filename += chamber + '/' + 'hel18_dwells_green_data.xlsx'
 
-            data = pd.read_excel(filename+path, index_col=[0, 1], dtype={'kon' :np.str})
-            dwells = data['offtime'].values
-            dwells = dwells[~np.isnan(dwells)]
-            hist = get_dwell_hist(dwells)
-            dwells_all.append(dwells)
+    data = pd.read_excel(filename, index_col=[0, 1], dtype={'kon' :np.str})
+    dwells = data.ontime.values[data.onside.values != 'r' ]
 
-    dwells_all = np.concatenate(dwells_all)
+    dwells = dwells[~np.isnan(dwells)]
+    hist = get_dwell_hist(dwells)
+    hist.set_c('green')
+
+
+#    plt.savefig(filename+'dwelltime_dist_log.png', dpi=200)
+
+
+#
+#if __name__ == '__main__':
+#    filename = 'G:/SM-data/20191101_dcas9_flow_DNA04_DNA20/'
+#    chamber = '#4.20_streptavidin_0.5nM_dcas9-crRNA-Cy5_10nM_DNA04-Cy3_G_flow'
+#    filename += chamber + '/'
+#    dwells_all = []
+#    for path in os.listdir(filename):
+#        if 'dwells' in path:
+#
+#            data = pd.read_excel(filename+path, index_col=[0, 1], dtype={'kon' :np.str})
+#            dwells = data['offtime'].values
+#            dwells = dwells[~np.isnan(dwells)]
+#            hist = get_dwell_hist(dwells)
+#            dwells_all.append(dwells)
+
+#    dwells_all = np.concatenate(dwells_all)
 
 #    hist = get_dwell_hist(dwells_all, extra_label='All: ')
 #    plt.savefig(filename+'dwelltime_dist.png', dpi=200)
 
-    hist = get_dwell_hist(dwells_all, extra_label='All: ', log=True)
-    plt.savefig(filename+'dwelltime_dist_log.png', dpi=200)
+#    hist = get_dwell_hist(dwells_all, extra_label='All: ', log=True)

trace_analysis/file.py
@@ -102,7 +102,7 @@ class File:
     def time(self):  # the time axis of the experiment, if not found in log it will be asked as input
         if self.exposure_time is None:
             self.exposure_time = input(f'Exposure time for {self.name}: ')
-        return np.arange(0, self.Nframes)*self.exposure_time
+        return np.arange(0, self.number_of_frames)*self.exposure_time
 
     @property
     def traces(self):