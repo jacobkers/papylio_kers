a087b6c55ad4526c5bf1461deb24d82555808a22
Iasonas Katechis <i.katechis@tudelft.nl>
2019-12-13 15:29:21 +0100

    Work in progress

--- GUI.py
+++ GUI.py
@@ -182,45 +182,12 @@ class MainFrame(wx.Frame):
 
 
 class DistributionsControlsPanel(wx.Frame):
-    def __init__(self, title='Plot and Fit dwelltime distributions', parent=None):
+    def __init__(self, parent=None, title='Plot and Fit dwelltime distributions'):
         wx.Frame.__init__(self, parent=parent, title=title)
         self.parent = parent
         panel = wx.Panel(self)
 
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        nm = wx.StaticBox(panel, -1, 'Name:')
-        nmSizer = wx.StaticBoxSizer(nm, wx.VERTICAL)
 
-        nmbox = wx.BoxSizer(wx.HORIZONTAL)
-        fn = wx.StaticText(panel, -1, "First Name")
-
-        nmbox.Add(fn, 0, wx.ALL|wx.CENTER, 5)
-        nm1 = wx.TextCtrl(panel, -1, style = wx.ALIGN_LEFT)
-        nm2 = wx.TextCtrl(panel, -1, style = wx.ALIGN_LEFT)
-        ln = wx.StaticText(panel, -1, "Last Name")
-
-        nmbox.Add(nm1, 0, wx.ALL|wx.CENTER, 5)
-        nmbox.Add(ln, 0, wx.ALL|wx.CENTER, 5)
-        nmbox.Add(nm2, 0, wx.ALL|wx.CENTER, 5)
-        nmSizer.Add(nmbox, 0, wx.ALL|wx.CENTER, 10)
-
-        sbox = wx.StaticBox(panel, -1, 'buttons:')
-        sboxSizer = wx.StaticBoxSizer(sbox, wx.VERTICAL)
-
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        okButton = wx.Button(panel, -1, 'ok')
-
-        hbox.Add(okButton, 0, wx.ALL|wx.LEFT, 10)
-        cancelButton = wx.Button(panel, -1, 'cancel')
-
-        hbox.Add(cancelButton, 0, wx.ALL|wx.LEFT, 10)
-        sboxSizer.Add(hbox, 0, wx.ALL|wx.LEFT, 10)
-        vbox.Add(nmSizer,0, wx.ALL|wx.CENTER, 5)
-        vbox.Add(sboxSizer,0, wx.ALL|wx.CENTER, 5)
-        panel.SetSizer(vbox)
-        self.Centre()
-
-        panel.Fit()
 
 
 
--- trace_analysis/analysis/plotDistributionPanel.py
+++ trace_analysis/analysis/plotDistributionPanel.py
@@ -9,59 +9,157 @@ Created on Thu Dec 12 01:02:04 2019
 
 import wx
 
+
+
 class Example(wx.Frame):
 
-   def __init__(self, parent, title):
-      super(Example, self).__init__(parent, title = title)
+    def __init__(self, parent, title='Plot and Fit dwelltime distributions'):
+        super(Example, self).__init__(parent, title=title)
 
-      self.InitUI()
-      self.Centre()
-      self.Show()
+        self.InitUI()
+        self.Centre()
 
-   def InitUI(self):
+    def InitUI(self):
 
-      panel = wx.Panel(self)
-      sizer = wx.GridBagSizer(0,0)
+        panel = wx.Panel(self)
+        #  Top sizer which places the different boxes vertically
+        topsizer = wx.BoxSizer(wx.VERTICAL)
 
-      text = wx.StaticText(panel, label = "Name:")
-      sizer.Add(text, pos = (0, 0), flag = wx.ALL, border = 5)
+        #  SizerFlags object for commonly used flags
+        flagsExpand = wx.SizerFlags(1)
+        flagsExpand.Expand().Border(wx.ALL, 10)
 
-      tc = wx.TextCtrl(panel)
-      sizer.Add(tc, pos = (0, 1), span = (1, 2), flag = wx.EXPAND|wx.ALL, border = 5)
+        flagsCenter = wx.SizerFlags(1)
+        flagsCenter.Align(wx.CENTER).Border(wx.ALL, 5)
 
-      text1 = wx.StaticText(panel, label = "address")
-      sizer.Add(text1, pos = (1, 0), flag = wx.ALL, border = 5)
+        flagsLeft = wx.SizerFlags(1)
+        flagsLeft.Align(wx.LEFT).Border(wx.ALL, 5)
+        # Static box for input of distribution plot and fit options
+        sboxConfig = wx.StaticBox(panel, label="Configuration")
+        sboxConfigSizer = wx.StaticBoxSizer(sboxConfig, wx.VERTICAL)
 
-      tc1 = wx.TextCtrl(panel,style = wx.TE_MULTILINE)
-      sizer.Add(tc1, pos = (1,1), span = (1, 3), flag = wx.EXPAND|wx.ALL, border = 5)
+        # Box to hold the distribution type choice
+        sBoxSizerSelect = wx.BoxSizer(wx.HORIZONTAL)
+        sBoxSizerSelect.Add(wx.StaticText(sboxConfig, label="Distribution: ",
+                                          style = wx.ALIGN_CENTRE),
+                            flagsCenter)
 
-      text2 = wx.StaticText(panel,label = "age")
-      sizer.Add(text2, pos = (2, 0), flag = wx.ALL, border = 5)
+        self.comboDist = wx.ComboBox(sboxConfig, value='offtimes',
+                                        choices=['offtimes', 'ontimes'])
+        sBoxSizerSelect.Add(self.comboDist, flagsCenter)
 
-      tc2 = wx.TextCtrl(panel)
-      sizer.Add(tc2, pos = (2,1), flag = wx.ALL, border = 5)
+        # GridBag Sizer to hold the parameter selection buttons
+        gridSizer = wx.GridBagSizer(5, 5)
+        # 1sr column, choose the trace type(s)
+        gridSizer.Add(wx.StaticText(sboxConfig, label='Trace:'),  pos=(0, 0),
+                      flag=wx.LEFT, border=5)
 
-      text3 = wx.StaticText(panel,label = "Mob.No")
-      sizer.Add(text3, pos = (2, 2), flag = wx.ALIGN_CENTER|wx.ALL, border = 5)
+        traces = ['red', 'green', 'total', 'FRET']
+        self.chbTraces = [wx.CheckBox(sboxConfig, label=l) for l in traces]
 
-      tc3 = wx.TextCtrl(panel)
-      sizer.Add(tc3, pos = (2,3),flag = wx.EXPAND|wx.ALL, border = 5)
+        for i, chb in enumerate(self.chbTraces):
+            gridSizer.Add(chb, pos=(i+1,0), flag=wx.LEFT, border=5)
 
-      text4 = wx.StaticText(panel, label = "Description")
-      sizer.Add(text4, pos = (3, 0), flag = wx.ALL, border = 5)
+        # 1sr column, choose the trace type(s)
+        gridSizer.Add(wx.StaticText(sboxConfig, label='Side:'),  pos=(0, 1),
+                      flag=wx.LEFT, border=5)
+        sides = ['left', 'middle', 'right']
+        self.chbSides = [wx.CheckBox(sboxConfig, label=l) for l in sides]
+        for i, chb in enumerate(self.chbSides):
+            gridSizer.Add(chb, pos=(i+1,1), flag=wx.LEFT, border=5)
 
-      tc4 = wx.TextCtrl(panel,style =  wx.TE_MULTILINE)
-      sizer.Add(tc4, pos = (3,1), span = (1,3), flag = wx.EXPAND|wx.ALL, border = 5)
-      sizer.AddGrowableRow(3)
 
-      buttonOk = wx.Button(panel, label = "Ok")
-      buttonClose = wx.Button(panel, label = "Close" )
 
-      sizer.Add(buttonOk, pos = (4, 2),flag = wx.ALL, border = 5)
-      sizer.Add(buttonClose, pos = (4, 3), flag = wx.ALL, border = 5)
 
-      panel.SetSizerAndFit(sizer)
 
-app = wx.App()
-Example(None, title = 'GridBag Demo')
-app.MainLoop()
+        sboxConfigSizer.Add(sBoxSizerSelect, 1, wx.EXPAND)
+        sboxConfigSizer.Add(gridSizer, 4, wx.EXPAND)
+
+
+
+        topsizer.Add(sboxConfigSizer, flagsExpand)
+
+        # topsizer.SetSizeHints(self)
+        panel.SetSizer(topsizer)
+        topsizer.Fit(self)
+
+
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
+
+        # sizer = wx.GridBagSizer(5, 5)
+
+        # text1 = wx.StaticText(panel, label="Java Class")
+        # sizer.Add(text1, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM,
+        #     border=15)
+
+        # icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap('exec.png'))
+        # sizer.Add(icon, pos=(0, 4), flag=wx.TOP|wx.RIGHT|wx.ALIGN_RIGHT,
+        #     border=5)
+
+        # line = wx.StaticLine(panel)
+        # sizer.Add(line, pos=(1, 0), span=(1, 5),
+        #     flag=wx.EXPAND|wx.BOTTOM, border=10)
+
+        # text2 = wx.StaticText(panel, label="Name")
+        # sizer.Add(text2, pos=(2, 0), flag=wx.LEFT, border=10)
+
+        # tc1 = wx.TextCtrl(panel)
+        # sizer.Add(tc1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)
+
+        # text3 = wx.StaticText(panel, label="Package")
+        # sizer.Add(text3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
+
+        # tc2 = wx.TextCtrl(panel)
+        # sizer.Add(tc2, pos=(3, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND,
+        #     border=5)
+
+        # button1 = wx.Button(panel, label="Browse...")
+        # sizer.Add(button1, pos=(3, 4), flag=wx.TOP|wx.RIGHT, border=5)
+
+        # text4 = wx.StaticText(panel, label="Extends")
+        # sizer.Add(text4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
+
+        # combo = wx.ComboBox(panel)
+        # sizer.Add(combo, pos=(4, 1), span=(1, 3),
+        #     flag=wx.TOP|wx.EXPAND, border=5)
+
+        # button2 = wx.Button(panel, label="Browse...")
+        # sizer.Add(button2, pos=(4, 4), flag=wx.TOP|wx.RIGHT, border=5)
+
+        # sb = wx.StaticBox(panel, label="Optional Attributes")
+
+        # boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
+        # boxsizer.Add(wx.CheckBox(panel, label="Public"),
+        #     flag=wx.LEFT|wx.TOP, border=5)
+        # boxsizer.Add(wx.CheckBox(panel, label="Generate Default Constructor"),
+        #     flag=wx.LEFT, border=5)
+        # boxsizer.Add(wx.CheckBox(panel, label="Generate Main Method"),
+        #     flag=wx.LEFT|wx.BOTTOM, border=5)
+        # sizer.Add(boxsizer, pos=(5, 0), span=(1, 5),
+        #     flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
+
+        # button3 = wx.Button(panel, label='Help')
+        # sizer.Add(button3, pos=(7, 0), flag=wx.LEFT, border=10)
+
+        # button4 = wx.Button(panel, label="Ok")
+        # sizer.Add(button4, pos=(7, 3))
+
+        # button5 = wx.Button(panel, label="Cancel")
+        # sizer.Add(button5, pos=(7, 4), span=(1, 1),
+        #     flag=wx.BOTTOM|wx.RIGHT, border=10)
+
+        # sizer.AddGrowableCol(2)
+
+        # panel.SetSizer(sizer)
+        # sizer.Fit(self)
