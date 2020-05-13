5ea1b68427dec5baeca7f62e68711fa4846b68c8
Ivo Severins <i.w.h.severins@tudelft.nl>
2020-05-13 12:43:39 +0200

    Add first version of SequencingDataMapping class

--- trace_analysis/plugins/sequencing/geometricHashing2.py
+++ trace_analysis/plugins/sequencing/geometricHashing2.py
@@ -101,8 +101,8 @@ def geometric_hash(point_set, maximum_distance=100, point_set_size=4):
 
     return point_set_KDTree, point_tuples, hash_table_KDTree
 
-def find_match_after_hashing(destination_KDTree, destination_tuples, destination_hash_table_KDTree,
-                             source_KDTree, source_tuples, source_hash_table_KDTree, source_bounds):
+def find_match_after_hashing(source_KDTree, source_tuples, source_hash_table_KDTree, source_bounds,
+                             destination_KDTree, destination_tuples, destination_hash_table_KDTree):
 
     for source_tuple_index in np.arange(len(source_tuples)):
         distance, destination_tuple_index = destination_hash_table_KDTree.query(
@@ -155,7 +155,7 @@ def find_match(source, destination, source_bounds):
     # source_KDTree, source_tuples, source_hash_table_KDTree = geometric_hash(source, 4, 4)
     # 200 points 200,20
     # 10000 points 10,1
-    return find_match_after_hashing(*geometric_hash(destination, 40, 4), *geometric_hash(source, 4, 4), source_bounds)
+    return find_match_after_hashing(*geometric_hash(source, 4, 4), source_bounds, *geometric_hash(destination, 40, 4))
 
 match = find_match(source, destination, source_bounds)
 scatter_coordinates([source,destination,match(source)])
@@ -169,8 +169,192 @@ scatter_coordinates([source,destination,match(source)])
 
 
 
+import pickle
+from pathlib2 import Path
+from trace_analysis.mapping.mapping import Mapping2
+from trace_analysis.coordinate_transformations import translate, rotate, magnify, reflect, transform
+from trace_analysis.plotting import plot_match
+
+class SequencingDataMapping:
+    def __init__(self, tile, files, dataPath):
+        self.tile = tile
+        self.files = files # List of coordinate sets
+        self.dataPath = Path(dataPath)
+
+        self.initial_image_transformation = {'reflection': 0, 'rotation': 0, 'magnification': 1} # This reflects with respect to axis 0.
+
+        # self.mode = mode
+        # if mode == 'translation': self.hashTableRange = [-10000, 10000]
+        # else: self.hashTableRange = [-1,1]
+        # self.nBins = nBins
+
+        self.bases_hashTable = 'all'
+        self.bases_findMatch = 20
+
+        self.tuple_size = 4
+        self.maximum_distance_tile = 40
+        self.maximum_distance_file = 4
+
+        self._hashTable = None
+        self._matches = None
+
+        # self.rotationRange = rotationRange
+        # self.magnificationRange = magnificationRange
 
 
+    @property
+    def hashTable(self):
+        if self._hashTable is None:
+            if self.dataPath.joinpath(self.tile.name+'.ht').is_file():
+                with self.dataPath.joinpath(self.tile.name+'.ht').open('rb') as f:
+                    self._hashTable = pickle.load(f)
+            else:
+                # self._hashTable = pointHash(self.tile.coordinates, bases=self.bases_hashTable, mode=self.mode,
+                #                             hashTableRange=self.hashTableRange, nBins=self.nBins,
+                #                             rotationRange=self.rotationRange, magnificationRange=self.magnificationRange)
+                self._hashTable = geometric_hash(self.tile.coordinates, self.maximum_distance_tile, self.tuple_size)
+                with self.dataPath.joinpath(self.tile.name+'.ht').open('wb') as f:
+                    pickle.dump(self._hashTable, f)
+        return self._hashTable
+
+    @property
+    def matches(self):
+        if self._matches is None:
+            if self.dataPath.joinpath(self.tile.name+'.matches').is_file():
+                with self.dataPath.joinpath(self.tile.name+'.matches').open('rb') as f:
+                    self._matches = pickle.load(f)
+            else:
+                self._matches = self.findMatches()
+                with self.dataPath.joinpath(self.tile.name+'.matches').open('wb') as f:
+                    pickle.dump(self._matches, f)
+        return self._matches
+
+    def save(self):
+        if self.dataPath.joinpath(self.tile.name+'.sm').is_file():
+            print('File already exists')
+        else:
+            with self.dataPath.joinpath(self.tile.name+'.sm').open('wb') as f:
+                pickle.dump(self, f)
+
+    @staticmethod
+    def load(path):
+        with path.open('rb') as f:
+            return pickle.load(f)
+
+    def findMatches(self):
+        matches = []
+        for file in self.files:
+            print(file.name)
+            coordinates, initial_transformation = transform(file.coordinates, returnTransformationMatrix = True,
+                                                            **self.initial_image_transformation)
+
+            file_hash = geometric_hash(coordinates, self.maximum_distance_file, self.tuple_size)
+
+            #TODO: Make file.bounds use consistent
+            match = find_match_after_hashing(*file_hash, file.bounds, *self.hashTable)
+            # match = findMatch(coordinates, self.hashTable,
+            #                                                           bases=self.bases_findMatch,
+            #                                                           returnMatchedBases=True,
+            #                                                           mode=self.mode,
+            #                                                           nBins=self.nBins,
+            #                                                           hashTableRange=self.hashTableRange,
+            #                                                           rotationRange=self.rotationRange,
+            #                                                           magnificationRange=self.magnificationRange)
+
+            transformation_matrix = match.params
+
+            transformation_matrix = transformation_matrix @ initial_transformation
+
+            match = Mapping2(file.coordinates.copy(), self.tile.coordinates.copy(),
+                                  method='geometric-hashing',
+                                  transformation_type='linear')
+
+            # match.name =
+            match.transformation = transformation_matrix
+            # match.best_image_basis = bestBasis
+            # match.count = bestBasis['counts']
+            # #match.image_coordinates_transformed = coordinates_transformed
+            # match.meanError = np.mean(
+            #     [np.min(np.linalg.norm(self.tile.coordinates - row, axis=1)) for row in coordinates_transformed])
+            # match.setWidth = np.linalg.norm(np.max(coordinates_transformed, axis=0) - np.min(coordinates_transformed, axis=0))
+            # match.percentMatch = bestBasis['counts'] / coordinates_transformed.shape[0]
+
+            matches.append(match)
+
+        return matches
+    #
+    # def histogram_matches(self, export = False):
+    #     counts = [match.count for match in self.matches]
+    #     fig, ax = plt.subplots(figsize = (6,3))
+    #     ax.hist(counts, np.arange(0.5, np.max(counts) + 1.5, 1), facecolor = 'k', rwidth = 0.5)
+    #
+    #     ax.set_xlim([0,np.max(counts)+1])
+    #
+    #     ax.set_xlabel('Number of matches for best matching basis')
+    #     ax.set_ylabel('Count')
+    #     plt.tight_layout()
+    #
+    #     if export:
+    #         fig.savefig(self.dataPath.joinpath('histogramNumberOfMatches.pdf'), bbox_inches='tight')
+    #         fig.savefig(self.dataPath.joinpath('histogramNumberOfMatches.png'), bbox_inches='tight')
+
+    def show_match(self, match, figure = None, view='destination'):
+        if not figure: figure = plt.gcf()
+        figure.clf()
+        ax = figure.gca()
+
+        #ax.scatter(ps2[:,0],ps2[:,1],c='g',marker = '+')
+
+        ax.scatter(match.destination[:,0],match.destination[:,1], marker = '.', facecolors = 'k', edgecolors='k')
+        ax.scatter(match.transform_source_to_destination[:,0],match.transform_source_to_destination[:,1],c='r',marker = 'x')
+
+        destination_basis_index = match.best_image_basis['hashTableBasis']
+        source_basis_index = match.best_image_basis['testBasis']
+        ax.scatter(match.destination[destination_basis_index, 0], match.destination[destination_basis_index, 1], marker='.', facecolors='g', edgecolors='g')
+        ax.scatter(match.transform_source_to_destination[source_basis_index, 0], match.transform_source_to_destination[source_basis_index, 1], c='g',
+                   marker='x')
+
+        ax.set_aspect('equal')
+        ax.set_title('Tile:' + self.tile.name +', File: ' + str(self.files[self.matches.index(match)].relativeFilePath))
+
+        if view == 'source':
+            maxs = np.max(match.transform_source_to_destination, axis=0)
+            mins = np.min(match.transform_source_to_destination, axis=0)
+            ax.set_xlim([mins[0], maxs[0]])
+            ax.set_ylim([mins[1], maxs[1]])
+        elif view == 'destination':
+            maxs = np.max(match.destination, axis=0)
+            mins = np.min(match.destination, axis=0)
+            ax.set_xlim([mins[0], maxs[0]])
+            ax.set_ylim([mins[1], maxs[1]])
+            # ax.set_xlim([0, 31000])
+            # ax.set_ylim([0, 31000])
+
+        name = str(self.files[self.matches.index(match)].relativeFilePath)
+        print(name)
+        n = name.replace('\\', '_')
+
+        figure.savefig(self.dataPath.joinpath(n + '_raw.pdf'), bbox_inches='tight')
+        figure.savefig(self.dataPath.joinpath(n + '_raw.png'), bbox_inches='tight', dpi=1000)
+
+    def plot_match(self, match):
+        name = str(self.files[self.matches.index(match)].relativeFilePath)
+        print(name)
+        plot_match(match, self.dataPath, name, unit='um')
+
+    def loop_through_matches(self, figure=plt.figure()):
+        plt.ion()
+        for match in self.matches:
+            self.show_match(match, figure=figure)
+            plt.show()
+            plt.pause(0.001)
+            input("Press enter to continue")
+
+    def give_matches_to_files(self, match_threshold = 0):
+        for file, match in zip(self.files, self.matches):
+            if match.count >= match_threshold:
+                file.sequence_match = match
+                file.sequence_match.tile = self.tile
 
 
 
