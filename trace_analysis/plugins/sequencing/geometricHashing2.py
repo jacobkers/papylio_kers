6ccc639d7af7b7ad25a3969ab673202cc9498912
Ivo Severins <i.w.h.severins@tudelft.nl>
2020-05-24 19:40:21 +0200

    Sequential find phase
    Geometic hashing algorithm split over additional functions. During the find phase there is no need anymore to calculate the entire source hash table, instead using an iterator, hash table values are generated one by one.

--- trace_analysis/plugins/sequencing/geometricHashing2.py
+++ trace_analysis/plugins/sequencing/geometricHashing2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.path as pth
-from scipy.spatial import KDTree
+from scipy.spatial import cKDTree
 import random
 from trace_analysis.mapping.geometricHashing import mapToPoint
 from trace_analysis.plotting import scatter_coordinates
@@ -42,80 +42,104 @@ def crop_coordinates(coordinates, vertices):
 # indices_in_between_pairs = centers_KDTree.query_ball_tree(destination_KDTree, r=distance/2*0.8)
 
 
-def geometric_hash(point_set, maximum_distance=100, point_set_size=4):
-    # TODO: Add minimum_distance and implement
-    # TODO: Make invariant to mirroring
-
-    point_set_KDTree = KDTree(point_set)
-
+def generate_point_tuples(point_set_KDTree, maximum_distance, tuple_size):
     pairs = list(point_set_KDTree.query_pairs(maximum_distance))
 
     point_tuples = []
-    hash_table = []
+
     for pair in pairs:
-        pair_coordinates = point_set[list(pair)]
+        pair_coordinates = point_set_KDTree.data[list(pair)]
         center = (pair_coordinates[0] + pair_coordinates[1]) / 2
-        distance = np.linalg.norm(pair_coordinates[0]-pair_coordinates[1])
-        internal_points = point_set_KDTree.query_ball_point(center, distance/2*0.99)
-        if len(internal_points) >= (point_set_size-2):
+        distance = np.linalg.norm(pair_coordinates[0] - pair_coordinates[1])
+        internal_points = point_set_KDTree.query_ball_point(center, distance / 2 * 0.99)
+        if len(internal_points) >= (tuple_size - 2):
             random.shuffle(internal_points)
-            internal_points = internal_points[0:(point_set_size-2)]
+            internal_points = internal_points[0:(tuple_size - 2)]
+            #point_tuples.append(pair + tuple(internal_points))
+            yield pair + tuple(internal_points)
 
-            internal_coordinates = point_set[internal_points]
-            # Sort internal points based on x coordinate
-            # internal_point_order = np.argsort(internal_coordinates[:, 0])
-            # internal_points = [internal_points[i] for i in internal_point_order]
-            # internal_coordinates = internal_coordinates[internal_point_order]
+    #return point_tuples
 
-            end_points = np.array([[0, 0], [1, 1]])
+def geometric_hash_table(point_set_KDTree, point_tuples, tuple_size):
+    #hash_table = []
+    for point_tuple in point_tuples:
+
+        pair = point_tuple[:2]
+        internal_points = point_tuple[2:]
+        pair_coordinates = point_set_KDTree.data[list(pair)]
+        internal_coordinates = point_set_KDTree.data[list(internal_points)]
+        # Sort internal points based on x coordinate
+        # internal_point_order = np.argsort(internal_coordinates[:, 0])
+        # internal_points = [internal_points[i] for i in internal_point_order]
+        # internal_coordinates = internal_coordinates[internal_point_order]
+
+        end_points = np.array([[0, 0], [1, 1]])
+        hash_coordinates = mapToPoint(internal_coordinates, pair_coordinates, end_points)
+        # Break similarity of pair
+        if np.sum(hash_coordinates[:, 0]) > ((tuple_size - 2) / 2):
+            pair = pair[::-1]
+            pair_coordinates = pair_coordinates[::-1]
             hash_coordinates = mapToPoint(internal_coordinates, pair_coordinates, end_points)
-            # Break similarity of pair
-            if np.sum(hash_coordinates[:,0]) > ((point_set_size-2)/2):
-                pair = pair[::-1]
-                pair_coordinates = pair_coordinates[::-1]
-                hash_coordinates = mapToPoint(internal_coordinates, pair_coordinates, end_points)
 
-            # Break similarity of internal points
-            internal_point_order = np.argsort(hash_coordinates[:, 0])
-            internal_points = [internal_points[i] for i in internal_point_order]
-            internal_coordinates = internal_coordinates[internal_point_order]
-            hash_coordinates = hash_coordinates[internal_point_order]
+        # Break similarity of internal points
+        internal_point_order = np.argsort(hash_coordinates[:, 0])
+        internal_points = [internal_points[i] for i in internal_point_order]
+        internal_coordinates = internal_coordinates[internal_point_order]
+        hash_coordinates = hash_coordinates[internal_point_order]
 
-            hash_code = hash_coordinates.flatten()
-            hash_table.append(hash_code)
+        point_tuple = pair + tuple(internal_points)
+        hash_code = hash_coordinates.flatten()
+        # hash_table.append(hash_code)
+        yield point_tuple, hash_code
 
-            point_tuples.append(pair + tuple(internal_points))
+        # plot_tuple(np.vstack([pair_coordinates, internal_coordinates])
+        # plot_tuple(np.vstack([end_points, hash_coordinates]))
 
-            # plot_tuple(np.vstack([pair_coordinates, internal_coordinates])
-            # plot_tuple(np.vstack([end_points, hash_coordinates]))
+def geometric_hash(point_set, maximum_distance=100, tuple_size=4):
+    # TODO: Add minimum_distance and implement
+    # TODO: Make invariant to mirroring
 
-    hash_table_KDTree = KDTree(np.array(hash_table))
+    point_set_KDTree = cKDTree(point_set)
+
+    point_tuple_generator = generate_point_tuples(point_set_KDTree, maximum_distance, tuple_size)
+
+    point_tuples = []
+    hash_table = []
+    for point_tuple, hash_code in geometric_hash_table(point_set_KDTree, point_tuple_generator, tuple_size):
+        point_tuples.append(point_tuple)
+        hash_table.append(hash_code)
+
+    hash_table_KDTree = cKDTree(np.array(hash_table))
 
     return point_set_KDTree, point_tuples, hash_table_KDTree
 
-def find_match_after_hashing(source_KDTree, source_tuples, source_hash_table_KDTree, source_vertices,
+def find_match_after_hashing(source, maximum_distance_source, tuple_size, source_vertices,
                              destination_KDTree, destination_tuples, destination_hash_table_KDTree):
 
-    for source_tuple_index in np.arange(len(source_tuples)):
-        distance, destination_tuple_index = destination_hash_table_KDTree.query(
-            source_hash_table_KDTree.data[source_tuple_index])
+    source_KDTree = cKDTree(source)
+    source_tuple_generator = generate_point_tuples(source_KDTree, maximum_distance_source, tuple_size)
+
+
+    # for source_tuple_index in np.arange(len(source_tuples)):
+    for source_tuple, source_hash_code in geometric_hash_table(source_KDTree, source_tuple_generator, tuple_size):
+        distance, destination_tuple_index = destination_hash_table_KDTree.query(source_hash_code)
         # We can also put a threshold on the distance here possibly
         # if distance < 0.01:
-        print(distance, source_tuple_index)
+        print(distance)
         # source_coordinate_tuple = source[list(source_tuples[source_tuple_index])]
         # destination_coordinate_tuple = destination[list(destination_tuples[destination_tuple_index])]
 
-        source_tuple = source_tuples[source_tuple_index]
+        # source_tuple = source_tuples[source_tuple_index]
         destination_tuple = destination_tuples[destination_tuple_index]
-        match = tuple_match(source_KDTree, destination_KDTree, source_vertices, source_tuple, destination_tuple)
+        match = tuple_match(source, destination_KDTree, source_vertices, source_tuple, destination_tuple)
         if match:
             return match
 
-def tuple_match(source_KDTree, destination_KDTree, source_vertices, source_tuple, destination_tuple):
-    source_coordinate_tuple = source_KDTree.data[list(source_tuple)]
+def tuple_match(source, destination_KDTree, source_vertices, source_tuple, destination_tuple):
+    source_coordinate_tuple = source[list(source_tuple)]
     destination_coordinate_tuple = destination_KDTree.data[list(destination_tuple)]
 
-    source_transformed, transformation_matrix = mapToPoint(source_KDTree.data, source_coordinate_tuple[:2], destination_coordinate_tuple[:2], returnTransformationMatrix=True)
+    source_transformed, transformation_matrix = mapToPoint(source, source_coordinate_tuple[:2], destination_coordinate_tuple[:2], returnTransformationMatrix=True)
     # scatter_coordinates([source_transformed])
 
     found_transformation = AffineTransform(transformation_matrix)
@@ -128,8 +152,8 @@ def tuple_match(source_KDTree, destination_KDTree, source_vertices, source_tuple
     pDB = 1 / source_transformed_area
 
     alpha = 0
-    # test_radius = 5
-    test_radius = 50
+    test_radius = 5
+    # test_radius = 50
     K=1
     K_threshold = 10e9
     for coordinate in source_transformed:
@@ -140,90 +164,95 @@ def tuple_match(source_KDTree, destination_KDTree, source_vertices, source_tuple
             return found_transformation
 
 
-
 def find_match(source, destination, source_vertices):
     # destination_KDTree, destination_tuples, destination_hash_table_KDTree = geometric_hash(destination, 40, 4)
     # source_KDTree, source_tuples, source_hash_table_KDTree = geometric_hash(source, 4, 4)
     # 200 points 200,20
     # 10000 points 10,1
-    return find_match_after_hashing(*geometric_hash(source, 4, 4), source_vertices, *geometric_hash(destination, 40, 4))
+    # return find_match_after_hashing(*geometric_hash(source, 4, 4), source_vertices, *geometric_hash(destination, 40, 4))
+    tuple_size = 4
+    maximum_distance_source = 4
+    maximum_distance_destination = 40
 
+    return find_match_after_hashing(source, maximum_distance_source, tuple_size, source_vertices,
+                                    *geometric_hash(destination, maximum_distance_destination, tuple_size))
 
 
 if __name__ == '__main__':
-    # # Make random source and destination dataset
-    # np.random.seed(42)
-    # destination = np.random.rand(1000,2)*1000
-    # source_vertices_in_destination = np.array([[300, 300], [450, 300], [450, 600], [300,600]])
-    #
-    #
-    # transformation = AffineTransform(scale=(0.1, 0.1), rotation=np.pi, shear=None, translation=(-100,350))
-    # source = transformation(crop_coordinates(destination, source_vertices_in_destination))
-    # source_vertices = transformation(source_vertices_in_destination)
-    # plt.figure()
-    # plt.scatter(destination[:,0],destination[:,1])
-    # plt.scatter(source[:,0],source[:,1])
-    # scatter_coordinates([source,destination,crop_coordinates(destination, source_vertices_in_destination)])
-    #
+    # Make random source and destination dataset
+    np.random.seed(42)
+    destination = np.random.rand(1000,2)*1000
+    source_vertices_in_destination = np.array([[300, 300], [450, 300], [450, 600], [300,600]])
+
+
+    transformation = AffineTransform(scale=(0.1, 0.1), rotation=np.pi, shear=None, translation=(-100,350))
+    source = transformation(crop_coordinates(destination, source_vertices_in_destination))
+    source_vertices = transformation(source_vertices_in_destination)
+    plt.figure()
+    plt.scatter(destination[:,0],destination[:,1])
+    plt.scatter(source[:,0],source[:,1])
+    scatter_coordinates([source,destination,crop_coordinates(destination, source_vertices_in_destination)])
+
     # destination_KDTree, destination_tuples, destination_hash_table_KDTree = geometric_hash(destination, 40, 4)
     # source_KDTree, source_tuples, source_hash_table_KDTree = geometric_hash(source, 4, 4)
     #
-    # match = find_match(source, destination, source_vertices)
-    # scatter_coordinates([source, destination, match(source), source_vertices,source_vertices_in_destination])
+    match = find_match(source, destination, source_vertices)
+    plt.figure()
+    scatter_coordinates([source, destination, match(source), source_vertices,source_vertices_in_destination])
 
-
-
-    file_coordinates = np.loadtxt(
-        r'C:\Users\Ivo Severins\Desktop\seqdemo\20190924 - Single-molecule setup (TIR-I)\16L\spool_6.pks')[:, 1:3]
-    tile_coordinates = np.loadtxt(r'C:\Users\Ivo Severins\Desktop\seqdemo\20190926 - Sequencer (MiSeq)\2102.loc')
-
-    source = file_coordinates
-    destination = tile_coordinates
-    source_vertices = np.array([[1024,0],[2048,0],[2048,2048],[1024,2048]])
-
-    # source = file_coordinates[[1,5,9,7]]
-    # destination = tile_coordinates[[73,82,85,84]]
-
-    source[:,0] = -source[:,0]
-    source_vertices[:,0] = -source_vertices[:,0]
-
-    # destination_KDTree, destination_tuples, destination_hash_table_KDTree = geometric_hash(destination, 3000, 4)
-    # source_KDTree, source_tuples, source_hash_table_KDTree = geometric_hash(source, 100, 4)
-
-    source_hash_data = geometric_hash(source, 1000, 4)
-    destination_hash_data = geometric_hash(destination, 3000, 4)
-
-    source_KDTree, source_tuples, source_hash_table_KDTree = source_hash_data
-    destination_KDTree, destination_tuples, destination_hash_table_KDTree = destination_hash_data
-
-    match = find_match_after_hashing(*source_hash_data, source_vertices, *destination_hash_data)
-
-    #match = find_match(source, destination, source_vertices)
-    #scatter_coordinates([source, destination, match(source), source_vertices, match(source_vertices)])
-
-
-
-    def scatter_with_number(coordinates):
-        # plot the chart
-        plt.scatter(coordinates[:,0], coordinates[:,1])
-
-        # zip joins x and y coordinates in pairs
-        for i, coordinate in enumerate(coordinates):
-            label = str(i)
-
-            # this method is called for each point
-            plt.annotate(label,  # this is the text
-                         (coordinate[0], coordinate[1]),  # this is the point to label
-                         textcoords="offset points",  # how to position the text
-                         xytext=(0, 10),  # distance from text to points (x,y)
-                         ha='center')
-
-    scatter_with_number(source)
-    scatter_with_number(destination)
-
-    for i, t in enumerate(source_tuples):
-        if ((1 in t) & (5 in t) & (9 in t) & (7 in t)):
-            print(i)
+    #
+    #
+    # file_coordinates = np.loadtxt(
+    #     r'C:\Users\Ivo Severins\Desktop\seqdemo\20190924 - Single-molecule setup (TIR-I)\16L\spool_6.pks')[:, 1:3]
+    # tile_coordinates = np.loadtxt(r'C:\Users\Ivo Severins\Desktop\seqdemo\20190926 - Sequencer (MiSeq)\2102.loc')
+    #
+    # source = file_coordinates
+    # destination = tile_coordinates
+    # source_vertices = np.array([[1024,0],[2048,0],[2048,2048],[1024,2048]])
+    #
+    # # source = file_coordinates[[1,5,9,7]]
+    # # destination = tile_coordinates[[73,82,85,84]]
+    #
+    # source[:,0] = -source[:,0]
+    # source_vertices[:,0] = -source_vertices[:,0]
+    #
+    # # destination_KDTree, destination_tuples, destination_hash_table_KDTree = geometric_hash(destination, 3000, 4)
+    # # source_KDTree, source_tuples, source_hash_table_KDTree = geometric_hash(source, 100, 4)
+    #
+    # source_hash_data = geometric_hash(source, 1000, 4)
+    # destination_hash_data = geometric_hash(destination, 3000, 4)
+    #
+    # source_KDTree, source_tuples, source_hash_table_KDTree = source_hash_data
+    # destination_KDTree, destination_tuples, destination_hash_table_KDTree = destination_hash_data
+    #
+    # match = find_match_after_hashing(*source_hash_data, source_vertices, *destination_hash_data)
+    #
+    # #match = find_match(source, destination, source_vertices)
+    # #scatter_coordinates([source, destination, match(source), source_vertices, match(source_vertices)])
+    #
+    #
+    #
+    # def scatter_with_number(coordinates):
+    #     # plot the chart
+    #     plt.scatter(coordinates[:,0], coordinates[:,1])
+    #
+    #     # zip joins x and y coordinates in pairs
+    #     for i, coordinate in enumerate(coordinates):
+    #         label = str(i)
+    #
+    #         # this method is called for each point
+    #         plt.annotate(label,  # this is the text
+    #                      (coordinate[0], coordinate[1]),  # this is the point to label
+    #                      textcoords="offset points",  # how to position the text
+    #                      xytext=(0, 10),  # distance from text to points (x,y)
+    #                      ha='center')
+    #
+    # scatter_with_number(source)
+    # scatter_with_number(destination)
+    #
+    # for i, t in enumerate(source_tuples):
+    #     if ((1 in t) & (5 in t) & (9 in t) & (7 in t)):
+    #         print(i)
 
 
     #
