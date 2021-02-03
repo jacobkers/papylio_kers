e68d915495ad00aa50d1f6cfebc29f3b3442ebdb
Ivo Severins <i.w.h.severins@tudelft.nl>
2021-02-03 17:03:17 +0100

    Add function for random selection from point set

--- trace_analysis/plugins/sequencing/point_set_simulation.py
+++ trace_analysis/plugins/sequencing/point_set_simulation.py
@@ -7,3 +7,25 @@ def simulate_point_set(number_of_points, bounds = [[0,1],[0,1]]):
     coordinates = unit_coordinates * (bounds[:,1]-bounds[:,0]) + bounds[:,0]
 
     return coordinates
+
+
+def random_selection_from_point_set(coordinates, fraction):
+    """ Obtain a random part of a point set
+    
+    Parameters
+    ----------
+    coordinates : Nx2 numpy.ndarray
+        Coordinates to sample from
+    fraction: float
+        Fraction of points that should be selected
+
+    Returns
+    -------
+    Nx2 numpy.ndarray
+        Selected coordinates
+    """
+
+    random_generator = np.random.default_rng()
+    size = round(fraction*len(coordinates))
+    return random_generator.choice(coordinates, size, replace=False, axis=0, shuffle=False)
+
