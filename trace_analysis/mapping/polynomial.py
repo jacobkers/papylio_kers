34ed75bd1f1a43b1cfa65dada5025fefc3c9db76
Ivo Severins <i.w.h.severins@tudelft.nl>
2021-06-02 16:12:43 +0200

    Make PolynomialTransform parameters equivalent to the skimage version
    Also add the previously forgotten init function.

--- trace_analysis/mapping/polynomial.py
+++ trace_analysis/mapping/polynomial.py
@@ -1,5 +1,7 @@
 import warnings
 import numpy as np
+import math
+from skimage.transform._geometric import GeometricTransform
 
 # from skimage.transform._geometric import PolynomialTransform
 #
@@ -13,10 +15,19 @@ import numpy as np
 #         coords = coords.astype(float)
 #         return super().__call__(coords)
 
-class PolynomialTransform:
+class PolynomialTransform(GeometricTransform):
+    def __init__(self, params=None):
+        if params is None:
+            # default to transformation which preserves original coordinates
+            params = np.array([[0, 1, 0], [0, 0, 1]])
+        if params.shape[0] != 2:
+            raise ValueError("invalid shape of transformation parameters")
+        self.params = params
+
     def estimate(self, source, destination, order=4):
         source = source.astype(float)
-        A = np.vstack([source[:,0] ** i * source[:, 1] ** j for i in range(order + 1) for j in range(order + 1)]).T
+        # A = np.vstack([source[:, 0] ** i * source[:, 1] ** j for i in range(order + 1) for j in range(order + 1)]).T
+        A = np.vstack([source[:, 0] ** (j - i) * source[:, 1] ** i for j in range(order+1) for i in range(j+1)]).T
         B = destination.astype(float)
 
         # Based on numpy.polyfit
@@ -29,12 +40,15 @@ class PolynomialTransform:
             msg = "Estimation may be poorly conditioned"
             warnings.warn(msg, np.RankWarning, stacklevel=4)
 
-        self.params = coeff
-        print(rank)
+        self.params = coeff.T
+        # print(rank)
         return True
 
     def __call__(self, coords):
-        order = int(np.sqrt(len(self.params)))-1
+        # order = int(np.sqrt(len(self.params)))-1
+        number_of_parameters = len(self.params.ravel())
+        order = int((- 3 + math.sqrt(9 - 4 * (2 - number_of_parameters))) / 2)
         coords = coords.astype(float)
-        A = np.vstack([coords[:, 0] ** i * coords[:, 1] ** j for i in range(order + 1) for j in range(order + 1)]).T
-        return A@self.params
+        # A = np.vstack([coords[:, 0] ** i * coords[:, 1] ** j for i in range(order + 1) for j in range(order + 1)]).T
+        A = np.vstack([coords[:, 0] ** (j - i) * coords[:, 1] ** i for j in range(order + 1) for i in range(j + 1)]).T
+        return A@self.params.T
