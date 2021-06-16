3bbe0ed35ea187663066406e50bca9e91f215a5f
Ivo Severins <i.w.h.severins@tudelft.nl>
2021-06-16 21:59:38 +0200

    Implement __getitem__, __add__, __radd__, import_traces_file and export_traces_file for new Molecules class

--- trace_analysis/molecule.py
+++ trace_analysis/molecule.py
@@ -1,10 +1,112 @@
 import numpy as np #scientific computing with Python
 import matplotlib.pyplot as plt #Provides a MATLAB-like plotting framework
+import pandas as pd
+from pathlib import Path
 from trace_analysis.analysis.autoThreshold import stepfinder
 # from trace_analysis.plugin_manager import PluginManager
 # from trace_analysis.plugin_manager import PluginMetaClass
 from trace_analysis.plugin_manager import plugins
 from trace_analysis.trace_extraction import make_gaussian
+import copy
+
+class Molecules:
+    def __init__(self, molecules=[], name=''):
+        # for value in molecules:
+        #     if not isinstance(value, Molecule):
+        #         raise TypeError('MoleculeList can only contain Molecule objects')
+        # self.molecules = molecules
+        self.name = name
+    #
+    # def __len__(self):
+    #     return self.sequence.shape[0]
+
+    def __getitem__(self, item):
+        # new = copy.copy(self)
+        new = Molecules(name=self.name)
+        for attribute, value in vars(self).items():
+            if isinstance(value, pd.DataFrame):
+                #new_value = value.xs(0, axis=1, level='Molecule', drop_level=False)
+                if isinstance(item, int):
+                    item = [item]
+                new.__setattr__(attribute, value.iloc[:, item])
+            elif isinstance(value, list):
+                new.__setattr__(attribute, value[item])
+            else:
+                new.__setattr__(attribute, value)
+        return new
+
+    def __add__(self, other):
+        new = Molecules(name=self.name)
+        for attribute, value in vars(self).items():
+            if isinstance(value, pd.DataFrame):
+                #new_value = value.xs(0, axis=1, level='Molecule', drop_level=False)
+                new_value = pd.concat([value, other.__getattribute__(attribute)], axis=1)
+                new.__setattr__(attribute, new_value)
+            elif isinstance(value, list):
+                new.__setattr__(attribute, value+other.__getattribute__(attribute))
+            else:
+                new.__setattr__(attribute, value)
+        return new
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+    # def __getattribute__(self, item):
+    #     try:
+    #         datatype = type(self.molecules[0].item)
+    #         if isinstance(datatype, pd.DataFrame):
+    #             [getattr(molecule, item) for molecule in self.molecules]
+    #     except AttributeError
+    #         super().__getattribute__(item)
+    #
+    #
+    # def __setattr__(self, key, value):
+    #     if isinstance(value, pd.DataFrame):
+    #         number_of_molecules = value.index.levshape[[name == 'Molecule' for name in value.index.names].index(True)]
+    #         if number_of_molecules != self.__len__():
+    #             raise ValueError('Wrong number of molecules')
+    #         for molecule in self.molecules:
+    #             molecule.key = value.loc[]
+
+    def import_traces_file(self, traces_filepath, number_of_channels):
+        self.traces = import_traces_file(traces_filepath, number_of_channels)
+
+    def export_traces_file(self, traces_filepath):
+        export_traces_file(self.traces, traces_filepath)
+
+
+
+def import_traces_file(traces_filepath, number_of_channels):
+    traces_filepath = Path(traces_filepath)
+    with traces_filepath.open('r') as traces_file:
+        number_of_frames = np.fromfile(traces_file, dtype=np.int32, count=1).item()
+        number_of_traces = np.fromfile(traces_file, dtype=np.int16, count=1).item()
+        number_of_molecules = number_of_traces // number_of_channels
+        rawData = np.fromfile(traces_file, dtype=np.int16, count=number_of_frames * number_of_traces)
+    # traces = np.reshape(rawData.ravel(),
+    #                         (number_of_channels, number_of_molecules, number_of_frames),
+    #                         order='F')  # 3d array of traces
+    traces = np.reshape(rawData.ravel(), (number_of_channels * number_of_molecules, number_of_frames),
+                        order='F')  # 2d array of traces
+
+    column_index = pd.MultiIndex.from_product([[traces_filepath.with_suffix('')],
+                                        np.arange(number_of_molecules),
+                                        np.arange(number_of_channels)],
+                                       names=['File', 'Molecule', 'Channel'])
+    index = pd.Index(data=np.arange(number_of_frames), name='Frame')
+    return pd.DataFrame(traces.T, index=index, columns=column_index).stack('Channel')
+
+def export_traces_file(traces, traces_filepath):
+    traces_filepath = Path(traces_filepath)
+    traces = traces.unstack('Channel')
+    with traces_filepath.open('w') as traces_file:
+        # Number of frames
+        np.array([len(traces.index)], dtype=np.int32).tofile(traces_file)
+        # Number of traces
+        np.array([len(traces.columns)], dtype=np.int16).tofile(traces_file)
+        traces.values.astype(np.int16).tofile(traces_file)
 
 @plugins
 class Molecule:
@@ -90,3 +192,14 @@ class Molecule:
             plt.savefig(f'{self.file.relativeFilePath}_mol_{self.index}.png', facecolor='white', dpi=300, transparent=True)
 
 
+
+
+
+
+if __name__ == '__main__':
+    traces_filepath = r'D:\SURFdrive\Promotie\Code\Python\traceAnalysis\twoColourExampleData\20141017 - Holliday junction - Copy\test.traces'
+    test = Molecules()
+    test.import_traces_file(traces_filepath, 2)
+    test[5:10]
+
+    test[5]+test[10]
