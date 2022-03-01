178348a02b58818a1426847e64949ca2c5239c29
Ivo Severins <i.w.h.severins@tudelft.nl>
2022-03-01 17:09:16 +0100

    Improve trace_extraction speed

--- trace_analysis/trace_extraction.py
+++ trace_analysis/trace_extraction.py
@@ -67,51 +67,127 @@ def make_gaussian_mask(size, center=None, offset=(0, 0), sigma=1.291):
 
     return mask
 
+# def extract_trace_values_from_image(image, coordinates, background, twoD_gaussians):  # extract traces
+#     coordinates = np.atleast_2d(coordinates)
+#
+#     # Probably indeed better to get this outside of the function, so that it is not redefined every time.
+#     half_size_Gaussian = len(twoD_gaussians[0]) // 2
+#
+#     # This should likely be put on a central place in selection of locations
+#     # coordinates = coordinates[self.is_within_margin(coordinates, edge = None, margin = half_size_Gaussian + 1)]
+#
+#     coordinates = np.floor(coordinates).astype(int)
+#
+#     trace_values = np.zeros(len(coordinates))
+#
+#     for i, coordinate in enumerate(coordinates):
+#         # First crop around spot, then do multiplication
+#         intensities = image[(coordinate[1] - half_size_Gaussian):(coordinate[1] + half_size_Gaussian + 1),
+#                       (coordinate[0] - half_size_Gaussian):(coordinate[0] + half_size_Gaussian + 1)
+#                       ]
+#
+#         intensities = intensities - background[i]
+#
+#         weighted_intensities = intensities * twoD_gaussians[i]
+#         trace_values[i] = np.sum(weighted_intensities)
+#         #trace_values[i]=np.sum(intensities) # MD testing
+#     return trace_values
+
+
 def extract_trace_values_from_image(image, coordinates, background, twoD_gaussians):  # extract traces
     coordinates = np.atleast_2d(coordinates)
 
     # Probably indeed better to get this outside of the function, so that it is not redefined every time.
-    half_size_Gaussian = len(twoD_gaussians[0]) // 2
+    half_size_gaussian = len(twoD_gaussians[0]) // 2
+    full_size_gaussian = half_size_gaussian * 2 + 1
 
     # This should likely be put on a central place in selection of locations
     # coordinates = coordinates[self.is_within_margin(coordinates, edge = None, margin = half_size_Gaussian + 1)]
 
     coordinates = np.floor(coordinates).astype(int)
 
-    trace_values = np.zeros(len(coordinates))
+    # im = np.arange(25).reshape(5, 5)
+    # coordinates = np.array([[1, 2], [2, 3]])
 
-    for i, coordinate in enumerate(coordinates):
-        # First crop around spot, then do multiplication
-        intensities = image[(coordinate[1] - half_size_Gaussian):(coordinate[1] + half_size_Gaussian + 1),
-                      (coordinate[0] - half_size_Gaussian):(coordinate[0] + half_size_Gaussian + 1)
-                      ]
+    x_indices = (coordinates[:, [0]] - half_size_gaussian) + np.atleast_2d(np.arange(0, full_size_gaussian))
+    y_indices = (coordinates[:, [1]] - half_size_gaussian) + np.atleast_2d(np.arange(0, full_size_gaussian))
 
-        intensities = intensities - background[i]
+    y_indices = y_indices[:, :, np.newaxis].repeat(full_size_gaussian, axis=2)
+    x_indices = x_indices[:, np.newaxis, :].repeat(full_size_gaussian, axis=1)
 
-        weighted_intensities = intensities * twoD_gaussians[i]
-        trace_values[i] = np.sum(weighted_intensities)
-        #trace_values[i]=np.sum(intensities) # MD testing
+    oneD_indices = (y_indices * image.shape[1] + x_indices).reshape(-1, full_size_gaussian ** 2)
+
+    intensities = image.take(oneD_indices).reshape(-1, full_size_gaussian, full_size_gaussian)
+
+    intensities = intensities - background[:,np.newaxis,np.newaxis]
+
+    weighted_intensities = intensities * twoD_gaussians
+    trace_values = weighted_intensities.sum(axis=(1,2))
+
     return trace_values
+
+# def extract_traces(movie, coordinates, background=None, channel='all', mask_size=1.291, neighbourhood_size=11):
+#     # return donor and acceptor for the full data set
+#     #     root, name = os.path.split(self.filepath)
+#     #     traces_fn=os.path.join(root,name[:-4]+'-P.traces')
+#
+#     # if os.path.isfile(traces_fn):
+#     # # load if traces file already exist
+#     #      with open(traces_fn, 'r') as infile:
+#     #          Nframes = np.fromfile(infile, dtype = np.int32, count = 1).item()
+#     #          Ntraces = np.fromfile(infile, dtype = np.int16, count = 1).item()
+#     #          rawData = np.fromfile(infile, dtype = np.int16, count = self.number_of_channels*Nframes * Ntraces)
+#     #      orderedData = np.reshape(rawData.ravel(), (self.number_of_channels, Ntraces//self.number_of_channels, Nframes), order = 'F')
+#     #      donor=orderedData[0,:,:]
+#     #      acceptor=orderedData[1,:,:]
+#     #      donor=np.transpose(donor)
+#     #      acceptor=np.transpose(acceptor)
+#     # else:
+#
+#     # go through all images, extract donor and acceptor signal
+#
+#     # This should likely be put on a central place in selection of locations
+#     # coordinates = coordinates[self.is_within_margin(coordinates, edge = None, margin = self.gauss_width // 2 + 1)]
+#
+#     # donor=np.zeros(( self.number_of_frames,self.pts_number))
+#     # acceptor=np.zeros((self.number_of_frames,self.pts_number))
+#     with movie:
+#         movie.read_header()
+#         traces = np.zeros((len(coordinates), movie.number_of_frames))
+#
+#         if background is None:
+#             background = np.zeros(len(coordinates))
+#
+#         # t0 = time.time()
+#
+#         #twoD_gaussian = make_gaussian(gauss_width, fwhm=3, center=(gauss_width // 2, gauss_width // 2))
+#
+#         offsets = coordinates % 1
+#         twoD_gaussians = [make_gaussian_mask(size=neighbourhood_size, offset=offsets[i], sigma=mask_size) for i in range(len(coordinates))]
+#
+#         for frame_number in range(movie.number_of_frames):  # self.number_of_frames also works for pm, len(self.movie_file_object.filelist) not
+#             # print(frame_number)
+#             if frame_number % 13 == 0:
+#                 sys.stdout.write(f'\r   Frame {frame_number} of {movie.number_of_frames}')
+#
+#             image = movie.read_frame(frame_number)
+#             image = movie.get_channel(image, channel)
+#
+#             trace_values_in_frame = extract_trace_values_from_image(image, coordinates, background, twoD_gaussians)
+#
+#             traces[:,frame_number] = trace_values_in_frame  # will multiply with gaussian, spot location is not drift compensated
+#         sys.stdout.write(f'\r   Frame {frame_number+1} of {movie.number_of_frames}\n')
+#         # t1=time.time()
+#         # elapsed_time=t1-t0; print(elapsed_time)
+#
+#         # root, name = os.path.split(self.filepath)
+#
+#         # if os.path.isfile(trace_fn):
+#
+#     return traces
 
 
 def extract_traces(movie, coordinates, background=None, channel='all', mask_size=1.291, neighbourhood_size=11):
-    # return donor and acceptor for the full data set
-    #     root, name = os.path.split(self.filepath)
-    #     traces_fn=os.path.join(root,name[:-4]+'-P.traces')
-
-    # if os.path.isfile(traces_fn):
-    # # load if traces file already exist
-    #      with open(traces_fn, 'r') as infile:
-    #          Nframes = np.fromfile(infile, dtype = np.int32, count = 1).item()
-    #          Ntraces = np.fromfile(infile, dtype = np.int16, count = 1).item()
-    #          rawData = np.fromfile(infile, dtype = np.int16, count = self.number_of_channels*Nframes * Ntraces)
-    #      orderedData = np.reshape(rawData.ravel(), (self.number_of_channels, Ntraces//self.number_of_channels, Nframes), order = 'F')
-    #      donor=orderedData[0,:,:]
-    #      acceptor=orderedData[1,:,:]
-    #      donor=np.transpose(donor)
-    #      acceptor=np.transpose(acceptor)
-    # else:
-
     # go through all images, extract donor and acceptor signal
 
     # This should likely be put on a central place in selection of locations
@@ -131,7 +207,7 @@ def extract_traces(movie, coordinates, background=None, channel='all', mask_size
         #twoD_gaussian = make_gaussian(gauss_width, fwhm=3, center=(gauss_width // 2, gauss_width // 2))
 
         offsets = coordinates % 1
-        twoD_gaussians = [make_gaussian_mask(size=neighbourhood_size, offset=offsets[i], sigma=mask_size) for i in range(len(coordinates))]
+        twoD_gaussians = np.stack([make_gaussian_mask(size=neighbourhood_size, offset=offsets[i], sigma=mask_size) for i in range(len(coordinates))])
 
         for frame_number in range(movie.number_of_frames):  # self.number_of_frames also works for pm, len(self.movie_file_object.filelist) not
             # print(frame_number)
@@ -140,6 +216,7 @@ def extract_traces(movie, coordinates, background=None, channel='all', mask_size
 
             image = movie.read_frame(frame_number)
             image = movie.get_channel(image, channel)
+
             trace_values_in_frame = extract_trace_values_from_image(image, coordinates, background, twoD_gaussians)
 
             traces[:,frame_number] = trace_values_in_frame  # will multiply with gaussian, spot location is not drift compensated
