13aebb11ed6dd304d7e0644c07e8f728e3c91c50
Ivo Severins <i.w.h.severins@tudelft.nl>
2022-06-05 12:48:16 +0200

    Add make_executable script

---
+++ Make_executable.cmd
@@ -0,0 +1,4 @@
+ECHO OFF
+CALL conda activate trace_analysis
+pyinstaller GUI.py --paths trace_analysis\mapping --hidden-import trace_analysis.plugins --collect-data distributed --onefile --add-data "trace_analysis\default_configuration.yml;."
+pause
