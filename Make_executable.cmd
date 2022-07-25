bddb0251cc4968a51ec41c4cfac28fc399910bad
Ivo Severins <i.w.h.severins@tudelft.nl>
2022-07-25 15:41:33 +0200

    make_executable into onefolder instead of onefile

--- Make_executable.cmd
+++ Make_executable.cmd
@@ -1,4 +1,4 @@
 ECHO OFF
 CALL conda activate trace_analysis
-pyinstaller GUI.py --paths trace_analysis\mapping --hidden-import trace_analysis.plugins --collect-data distributed --onefile --add-data "trace_analysis\default_configuration.yml;."
+pyinstaller GUI.py --paths trace_analysis\mapping --hidden-import trace_analysis.plugins --collect-data distributed --onefolder --add-data "trace_analysis\default_configuration.yml;."
 pause
