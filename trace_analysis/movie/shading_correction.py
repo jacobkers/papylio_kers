b16bf9058343d64ee8bb4a9f743d76ec93534fb1
Ivo Severins <i.w.h.severins@tudelft.nl>
2022-08-12 13:56:27 +0200

    Move shading correction

--- trace_analysis/movie/shading_correction.py
+++ trace_analysis/movie/shading_correction.py
@@ -8,6 +8,9 @@ import cv2
 import numpy as np
 from scipy.fftpack import dctn, idctn
 import tqdm
+from numba import njit
+
+https://github.com/linum-uqam/PyBaSiC
 
 class BaSiC(object):
     def __init__(self, input, estimate_darkfield=False, extension=".tif", verbose=False):
@@ -400,101 +403,361 @@ def inexact_alm_l1(imgs, l_s, l_d, tol=1e-6, maxIter=500, weight=1, estimateDark
     return Ib, Ir, D_field
 
 
-def BaSiC_basefluor(IF, flatfield, darkfield=None):
-    img_stack = IF
-    nrows = 128
-    ncols = 128
-    working_size = 128
-    n_images = img_stack.shape[0]
 
 
-    image_shape = img_stack.shape[1::]
-    new_shape = tuple([working_size] * 2)
-    img_stack_p = np.zeros([n_images, *new_shape], dtype=img_stack.dtype)
-    if working_size > image_shape[0]:
-        interpolation = cv2.INTER_LINEAR
-    else:
-        interpolation = cv2.INTER_AREA
-    for i in range(n_images):
-        img = img_stack[i, ...].squeeze()
-        img_stack_p[i, ...] = cv2.resize(img.T, new_shape, interpolation=interpolation).T
-    img_stack_resized = img_stack_p.astype(np.float32)
-    D = img_stack_resized.reshape(-1,nrows*ncols)
 
 
-    flatfield = cv2.resize(flatfield.T, new_shape, interpolation=interpolation).T
+
+
+
+
+@njit
+def mean_axis_0(arr):
+    return np.array([arr[..., i].mean() for i in range(arr.shape[1])])
+
+def get_photobleach(imgflt_stack, flatfield, darkfield=None, size=(128,128)):
+    # Size is (x,y)
+    img_stack = np.zeros((size[1], size[0], imgflt_stack.shape[0]))
+
+    for i, frame in enumerate(imgflt_stack):
+        img_stack[:, :, i] = cv2.resize(frame, size, interpolation=cv2.INTER_LINEAR).astype(np.float64)
+
+    imgflt_stack = img_stack
+
+    flatfield_small = cv2.resize(flatfield, size, interpolation=cv2.INTER_LINEAR).astype(np.float64)
     if darkfield is not None:
-        darkfield = cv2.resize(darkfield.T, new_shape, interpolation=interpolation).T
+        darkfield_small = cv2.resize(darkfield, size, interpolation=cv2.INTER_LINEAR).astype(np.float64)
 
-    weight = np.ones(D.shape)
-    eplson = 0.1
-    tol = 1e-6
+    imgflt_stack = np.reshape(imgflt_stack, (size[0] * size[1], -1)).astype(np.float64)
 
-    for reweighting_iter in range(5):
-        W_idct_hat = flatfield.flatten()
-        A_offset = darkfield.flatten()
-        A1_coeff = np.mean(D, 0)
-        # main iteration loop starts
-        temp = np.linalg.svd(D, compute_uv=False)
-        norm_two = temp[0]
-        mu = 12.5 / norm_two # this one can be tuned
-        mu_bar = mu * 1e7
-        rho = 1.5 # this one can be tuned
-        d_norm = np.linalg.norm(D, 'fro')
+    imgflt_stack_svd = np.linalg.svd(imgflt_stack, full_matrices=False, compute_uv=False)
+
+    return _get_photobleach(imgflt_stack, imgflt_stack_svd, flatfield_small, darkfield_small)
+
+# From https://github.com/PolusAI/polus-plugins/tree/dev/regression/polus-basic-flatfield-correction-plugin
+@njit
+def _get_photobleach(imgflt_stack, imgflt_stack_svd, flatfield, darkfield=None):
+    """Calculate the global effect of photobleaching for each image
+    Using the original data, flatfield, and darkfield images, estimate the total
+    contribution of photobleaching to an image in a series of images.
+    Inputs:
+        imgflt_stack - Numpy stack of images
+        flatfield - numpy floating precision matrix containing flatfield values
+        darkfield - numpy floating precision matrix containing darkfield values
+    Outputs:
+        A_coeff - A 1xn matrix of photobleaching offsets, where n is the number
+            of input images
+    """
+
+
+    # Initialize matrices
+    # Initialize matrices
+    # imgflt_stack = np.reshape(
+    #     imgflt_stack, (size * size, -1)
+    # ).astype(np.float64)
+    if darkfield is None:
+        darkfield = np.zeros(flatfield.shape, dtype=np.float64)
+
+    # Initialize weights and tolerances
+    weights = np.ones(imgflt_stack.shape, dtype=np.float64)
+    epsilon = np.float64(0.1)
+    tol = np.float64(10 ** -6)
+
+    # Run optimization exactly 5 times
+    for r in range(5):
+        # print(r)
+        # Calculate weights, offsets and coefficients
+        W_idct_hat = np.reshape(flatfield, (-1, 1))
+        A_offset = np.reshape(darkfield, (-1, 1))
+        A_coeff = np.reshape(mean_axis_0(imgflt_stack), (1, -1))
+
+        # Initialization values and learning rates
+        # temp = np.linalg.svd(imgflt_stack, full_matrices=False, compute_uv=False)
+        temp = imgflt_stack_svd
+        norm_two = np.float64(temp[0])
+        mu = np.float64(12.5) / norm_two
+        mu_bar = mu * 10 ** 7
+        rho = np.float64(1.5)
+
         ent1 = 1
-        iter = 0
-        total_svd = 0
+
+        # Normalization factors
+        d_norm = np.linalg.norm(imgflt_stack)#, "fro")
+
+        # Initialize augmented representation and error
+        A = np.zeros(imgflt_stack.shape, dtype=np.float64)
+        E1 = np.zeros(imgflt_stack.shape, dtype=np.float64)
+        # Y1 = np.float64(0)
+        Y1 = np.zeros(imgflt_stack.shape)
+
+        # Run optimization
+        iternum = 0
         converged = False
-        A1_hat = np.zeros(D.shape)
-        E1_hat = np.zeros(D.shape)
-        Y1 = 0
-
         while not converged:
-            iter = iter + 1
-            A1_hat = W_idct_hat * A1_coeff + A_offset
+            iternum += 1
+            # print(iternum)
+            # Calculate augmented representation
+            # A = np.matmul(W_idct_hat, A_coeff) + A_offset
+            A = (W_idct_hat * A_coeff) + A_offset
 
-            # update E1 using l0 norm
-            E1_hat = E1_hat + (D - A1_hat - E1_hat + (1 / mu) * Y1) / ent1
-            E1_hat = np.maximum(E1_hat - weight / (ent1 * mu), 0) + np.minimum(E1_hat + weight / (ent1 * mu), 0)
-            # update A1_coeff, A2_coeff and A_offset
-            # if coeff_flag
+            # Calculate errors
+            # E1 = E1 + np.divide(imgflt_stack - A - E1 + np.multiply(1 / mu, Y1), ent1)
+            E1 = imgflt_stack - A + Y1 / mu
 
-            # R1 = bsxfun( @ minus, D - E1_hat, A_offset)
-            R1 = D - E1_hat - A_offset
-            A1_coeff = np.mean(R1,0) - np.mean(A_offset)
-            # [A1_mincoeff, A1_mincoefftile] = min(A1_coeff);
-            # A1_back(A1_coeff < 1) = 1;
-            A1_coeff[A1_coeff < 0] = 0
+            # E1 = np.max(
+            #     np.reshape(
+            #         E1 - weights / (ent1 * mu),
+            #         (imgflt_stack.shape[0], imgflt_stack.shape[1], 1),
+            #     ),
+            #     -1,
+            #     initial=10 ** -6,
+            # ) + np.min(
+            #     np.reshape(
+            #         E1 + weights / (ent1 * mu),
+            #         (imgflt_stack.shape[0], imgflt_stack.shape[1], 1),
+            #     ),
+            #     -1,
+            #     initial=0,
+            # )
 
-            # A_offset = max(min(A_offset, A_uplimit), 0);
-            Z1 = D - A1_hat - E1_hat
-            # Z2 = D - A2_hat - E2_hat;
+            wem = weights / (ent1 * mu)
+            E1 = np.maximum(E1 - wem, 10 ** -6) + np.minimum(E1 + wem, 0)
 
+            # Calculate coefficients
+            R1 = imgflt_stack - E1
+            # A_coeff = np.reshape(np.mean(R1, axis=0), (1, -1)) - np.mean(A_offset)
+            A_coeff = np.reshape(mean_axis_0(R1), (1, -1)) - np.mean(A_offset)
+            # A_coeff[A_coeff < 0] = 0  # pixel values are never negative
+            A_coeff = np.maximum(A_coeff, 0)
+
+            # Loss
+            Z1 = imgflt_stack - A - E1
+
+            # Error updates
             Y1 = Y1 + mu * Z1
-            # Y2 = Y2 + mu * Z2;
 
+            # Update learning rate
+            # mu = np.min(mu * rho, initial=mu_bar)
             mu = np.minimum(mu * rho, mu_bar)
 
-            # stop Criterion
-            stopCriterion = np.linalg.norm(Z1, 'fro') / d_norm
-            # stopCriterion = max(norm(Z1, 'fro') / d_norm, norm(Z2, 'fro') / d_norm);
+            # Stop if below threshold
+            #stopCriterion = np.linalg.norm(Z1, "fro") / d_norm
+            stopCriterion = np.linalg.norm(Z1) / d_norm
+
+            # print(stopCriterion)
             if stopCriterion < tol:
                 converged = True
 
-            if total_svd % 10 == 0:
-                print('Iteration ' + str(iter) + ' |E1|_0 ' + str(np.sum(np.abs(E1_hat.flatten()) > 0)) + \
-                ' stopCriterion ' + str(stopCriterion))
+        # Update weights
+        # XE_norm = np.reshape(np.mean(A, axis=0), (1, -1)) / E1
+        XE_norm = np.reshape( mean_axis_0(A), (1, -1)) / E1
+        weights = 1 / np.abs(XE_norm + epsilon)
+        weights = weights * weights.size / np.sum(weights)
 
-        # update weight
-        # XE_norm = bsxfun( @ ldivide, E1_hat, mean(A1_hat));
-        XE_norm = np.mean(A1_hat) / E1_hat
-        weight = 1 / (np.abs(XE_norm) + eplson)
+    return A_coeff
 
-        # % if isempty(segmentation)
-        #     % weight(segmentation
-        #     ~ = 0)=0;
-        # % end
-        weight = weight * weight.size / weight.sum()
 
-    return A1_coeff
+#
+# #original
+#
+# OPTIONS = {'size':128}
+# def _get_photobleach(imgflt_stack, flatfield, darkfield=None):
+#     """Calculate the global effect of photobleaching for each image
+#     Using the original data, flatfield, and darkfield images, estimate the total
+#     contribution of photobleaching to an image in a series of images.
+#     Inputs:
+#         imgflt_stack - Numpy stack of images
+#         flatfield - numpy floating precision matrix containing flatfield values
+#         darkfield - numpy floating precision matrix containing darkfield values
+#     Outputs:
+#         A_coeff - A 1xn matrix of photobleaching offsets, where n is the number
+#             of input images
+#     """
+#     # Initialize matrices
+#     imgflt_stack = np.reshape(
+#         imgflt_stack, (OPTIONS["size"] * OPTIONS["size"], -1)
+#     ).astype(np.float64)
+#     if darkfield is None:
+#         darkfield = np.zeros(flatfield.shape, dtype=np.float64)
+#
+#     # Initialize weights and tolerances
+#     weights = np.ones(imgflt_stack.shape, dtype=np.float64)
+#     epsilon = np.float64(0.1)
+#     tol = np.float64(10 ** -6)
+#
+#     # Run optimization exactly 5 times
+#     for r in range(5):
+#         # Calculate weights, offsets and coefficients
+#         W_idct_hat = np.reshape(flatfield, (-1, 1))
+#         A_offset = np.reshape(darkfield, (-1, 1))
+#         A_coeff = np.reshape(np.mean(imgflt_stack, 0), (1, -1))
+#
+#         # Initialization values and learning rates
+#         temp = np.linalg.svd(imgflt_stack, full_matrices=False, compute_uv=False)
+#         norm_two = np.float64(temp[0])
+#         mu = np.float64(12.5) / norm_two
+#         mu_bar = mu * 10 ** 7
+#         rho = np.float64(1.5)
+#         ent1 = 1
+#
+#         # Normalization factors
+#         d_norm = np.linalg.norm(imgflt_stack, "fro")
+#
+#         # Initialize augmented representation and error
+#         A = np.zeros(imgflt_stack.shape, dtype=np.float64)
+#         E1 = np.zeros(imgflt_stack.shape, dtype=np.float64)
+#         Y1 = np.float64(0)
+#
+#         # Run optimization
+#         iternum = 0
+#         converged = False
+#         while not converged:
+#             iternum += 1
+#
+#             # Calculate augmented representation
+#             A = np.matmul(W_idct_hat, A_coeff) + A_offset
+#
+#             # Calculate errors
+#             E1 = E1 + np.divide(imgflt_stack - A - E1 + np.multiply(1 / mu, Y1), ent1)
+#             E1 = np.max(
+#                 np.reshape(
+#                     E1 - weights / (ent1 * mu),
+#                     (imgflt_stack.shape[0], imgflt_stack.shape[1], 1),
+#                 ),
+#                 -1,
+#                 initial=10 ** -6,
+#             ) + np.min(
+#                 np.reshape(
+#                     E1 + weights / (ent1 * mu),
+#                     (imgflt_stack.shape[0], imgflt_stack.shape[1], 1),
+#                 ),
+#                 -1,
+#                 initial=0,
+#             )
+#
+#             # Calculate coefficients
+#             R1 = imgflt_stack - E1
+#             A_coeff = np.reshape(np.mean(R1, 0), (1, -1)) - np.mean(A_offset)
+#             A_coeff[A_coeff < 0] = 0  # pixel values are never negative
+#
+#             # Loss
+#             Z1 = imgflt_stack - A - E1
+#
+#             # Error updates
+#             Y1 = Y1 + mu * Z1
+#
+#             # Update learning rate
+#             mu = np.min(mu * rho, initial=mu_bar)
+#
+#             # Stop if below threshold
+#             stopCriterion = np.linalg.norm(Z1, "fro") / d_norm
+#             if stopCriterion < tol:
+#                 converged = True
+#
+#         # Update weights
+#         XE_norm = np.reshape(np.mean(A, 0), (1, -1)) / E1
+#         weights = 1 / np.abs(XE_norm + epsilon)
+#         weights = weights * weights.size / np.sum(weights)
+#
+#     return A_coeff
 
+
+# Implemented based on Matlab code https://github.com/marrlab/BaSiC
+# def BaSiC_basefluor(IF, flatfield, darkfield=None):
+#     img_stack = IF
+#     nrows = 128
+#     ncols = 128
+#     working_size = 128
+#     n_images = img_stack.shape[0]
+#
+#
+#     image_shape = img_stack.shape[1::]
+#     new_shape = tuple([working_size] * 2)
+#     img_stack_p = np.zeros([n_images, *new_shape], dtype=img_stack.dtype)
+#     if working_size > image_shape[0]:
+#         interpolation = cv2.INTER_LINEAR
+#     else:
+#         interpolation = cv2.INTER_AREA
+#     for i in range(n_images):
+#         img = img_stack[i, ...].squeeze()
+#         img_stack_p[i, ...] = cv2.resize(img.T, new_shape, interpolation=interpolation).T
+#     img_stack_resized = img_stack_p.astype(np.float32)
+#     D = img_stack_resized.reshape(-1,nrows*ncols)
+#
+#
+#     flatfield = cv2.resize(flatfield.T, new_shape, interpolation=interpolation).T
+#     if darkfield is not None:
+#         darkfield = cv2.resize(darkfield.T, new_shape, interpolation=interpolation).T
+#
+#     weight = np.ones(D.shape)
+#     eplson = 0.1
+#     tol = 1e-6
+#
+#     for reweighting_iter in range(5):
+#         W_idct_hat = flatfield.flatten()
+#         A_offset = darkfield.flatten()
+#         A1_coeff = np.mean(D, 0)
+#         # main iteration loop starts
+#         temp = np.linalg.svd(D, compute_uv=False)
+#         norm_two = temp[0]
+#         mu = 12.5 / norm_two # this one can be tuned
+#         mu_bar = mu * 1e7
+#         rho = 1.5 # this one can be tuned
+#         d_norm = np.linalg.norm(D, 'fro')
+#         ent1 = 1
+#         iter = 0
+#         total_svd = 0
+#         converged = False
+#         A1_hat = np.zeros(D.shape)
+#         E1_hat = np.zeros(D.shape)
+#         Y1 = 0
+#
+#         while not converged:
+#             iter = iter + 1
+#             A1_hat = W_idct_hat * A1_coeff + A_offset
+#
+#             # update E1 using l0 norm
+#             E1_hat = E1_hat + (D - A1_hat - E1_hat + (1 / mu) * Y1) / ent1
+#             E1_hat = np.maximum(E1_hat - weight / (ent1 * mu), 0) + np.minimum(E1_hat + weight / (ent1 * mu), 0)
+#             # update A1_coeff, A2_coeff and A_offset
+#             # if coeff_flag
+#
+#             # R1 = bsxfun( @ minus, D - E1_hat, A_offset)
+#             R1 = D - E1_hat - A_offset
+#             A1_coeff = np.mean(R1,0) - np.mean(A_offset)
+#             # [A1_mincoeff, A1_mincoefftile] = min(A1_coeff);
+#             # A1_back(A1_coeff < 1) = 1;
+#             A1_coeff[A1_coeff < 0] = 0
+#
+#             # A_offset = max(min(A_offset, A_uplimit), 0);
+#             Z1 = D - A1_hat - E1_hat
+#             # Z2 = D - A2_hat - E2_hat;
+#
+#             Y1 = Y1 + mu * Z1
+#             # Y2 = Y2 + mu * Z2;
+#
+#             mu = np.minimum(mu * rho, mu_bar)
+#
+#             # stop Criterion
+#             stopCriterion = np.linalg.norm(Z1, 'fro') / d_norm
+#             # stopCriterion = max(norm(Z1, 'fro') / d_norm, norm(Z2, 'fro') / d_norm);
+#             if stopCriterion < tol:
+#                 converged = True
+#
+#             if total_svd % 10 == 0:
+#                 print('Iteration ' + str(iter) + ' |E1|_0 ' + str(np.sum(np.abs(E1_hat.flatten()) > 0)) + \
+#                 ' stopCriterion ' + str(stopCriterion))
+#
+#         # update weight
+#         # XE_norm = bsxfun( @ ldivide, E1_hat, mean(A1_hat));
+#         XE_norm = np.mean(A1_hat) / E1_hat
+#         weight = 1 / (np.abs(XE_norm) + eplson)
+#
+#         # % if isempty(segmentation)
+#         #     % weight(segmentation
+#         #     ~ = 0)=0;
+#         # % end
+#         weight = weight * weight.size / weight.sum()
+#
+#     return A1_coeff
+#
