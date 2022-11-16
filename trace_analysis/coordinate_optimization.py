83c49b7b210b5206dd33afd188c78f067c91e56d
Ivo Severins <i.w.h.severins@tudelft.nl>
2022-11-16 09:52:40 +0100

    Update coordinate_optimization.py

--- trace_analysis/coordinate_optimization.py
+++ trace_analysis/coordinate_optimization.py
@@ -18,6 +18,24 @@ def coordinates_within_margin_selection(coordinates,  image = None, bounds = Non
 
     return criteria.all(axis=0)
 
+def coordinates_within_margin_selection(coordinates,  image = None, bounds = None, margin=10):
+    if coordinates.size == 0:
+        return np.array([])
+
+    if image is not None:
+        bounds = np.array([[0,0], [image.shape[1],image.shape[0]]])
+
+    if isinstance(margin, int):
+        margin = np.array([margin, margin])
+
+    criteria = np.array([(coordinates[:, 0] > (bounds[0, 0] + margin[0])),
+                         (coordinates[:, 0] < (bounds[1, 0] - margin[0])),
+                         (coordinates[:, 1] > (bounds[0, 1] + margin[1])),
+                         (coordinates[:, 1] < (bounds[1, 1] - margin[1]))
+                         ])
+
+    return criteria.all(axis=0)
+
 
 def coordinates_within_margin(coordinates, image=None, bounds=None, margin=10):
     criteria = coordinates_within_margin_selection(coordinates,  image=image, bounds=bounds, margin=margin)
