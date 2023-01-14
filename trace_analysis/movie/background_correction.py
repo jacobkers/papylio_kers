2e43654cd288fa9b3bd3a4578bf8a5b6842e308f
Ivo Severins <i.w.h.severins@tudelft.nl>
2023-01-14 10:28:25 +0100

    Update fit_background_peak option in temporal background determination
    The fit now estimates the standard deviation of the gaussian, so the fitting is more robust.

--- trace_analysis/movie/background_correction.py
+++ trace_analysis/movie/background_correction.py
@@ -84,7 +84,9 @@ def gaussian_maximum_fit(frame, width_around_peak_fitted=200):
     frame_min = np.floor(frame.min()).astype(int)
     frame_max = np.ceil(frame.max()).astype(int)
 
-    count, edges = np.histogram(frame.flatten(), bins=frame_max - frame_min + 1, range=(frame_min - 0.5, frame_max + 0.5))
+    frame_flattened = frame.flatten()
+
+    count, edges = np.histogram(frame_flattened, bins=frame_max - frame_min + 1, range=(frame_min - 0.5, frame_max + 0.5))
 
     bincenters = (edges[:-1] + edges[1:]) / 2
     max_bin_center = bincenters[count.argmax()]
@@ -95,7 +97,9 @@ def gaussian_maximum_fit(frame, width_around_peak_fitted=200):
     x = bincenters[selection]
     y = count[selection]
 
-    popt, pcov = scipy.optimize.curve_fit(gauss_function, x, y, p0=[count.max(), max_bin_center, 100])#, maxfev=2000)
+    std = np.std(frame_flattened[(frame_flattened > x.min()) & (frame_flattened < x.max())])
+
+    popt, pcov = scipy.optimize.curve_fit(gauss_function, x, y, p0=[count.max(), max_bin_center, std], maxfev=2000)
     return popt[1]
 
 
