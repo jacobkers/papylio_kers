ec6f765fead52b6ecfb8e55a89e1b0899be0a519
Ivo Severins <i.w.h.severins@tudelft.nl>
2023-01-14 11:28:26 +0100

    Update Mapping2 cross-correlation plotting

--- trace_analysis/mapping/cross_correlation.py
+++ trace_analysis/mapping/cross_correlation.py
@@ -46,18 +46,25 @@ def coordinates_to_image(coordinates, gaussian_width=7, divider=5):
 
     return image_with_gaussians, transformation
 
-def cross_correlate(source, destination, gaussian_width=7, divider=5, plot=False, axes=None):
+def cross_correlate(source, destination, gaussian_width=7, divider=5, subtract_background=True, plot=False, axes=None):
     pseudo_image_source, transfomation_source = coordinates_to_image(source, gaussian_width=gaussian_width, divider=divider) #/ 5)
     pseudo_image_destination, transfomation_destination = coordinates_to_image(destination, gaussian_width=gaussian_width, divider=divider) #/ 5)
 
     from scipy.signal import correlate, correlation_lags
 
-    correlation = correlate(pseudo_image_destination, pseudo_image_source, mode='full')
+    correlation_raw = correlate(pseudo_image_destination, pseudo_image_source, mode='full')
+
+    if subtract_background:
+        import scipy.ndimage.filters as filters
+        correlation = correlation_raw - filters.minimum_filter(correlation_raw, 2 * gaussian_width)
+    else:
+        correlation = correlation_raw
+    # np.min(correlation.shape) / 200)
 
     if plot:
         if axes is None:
             axes = []
-            for i in range(3):
+            for i in range(4):
                 figure, axis = plt.subplots()
                 axes.append(axis)
 
@@ -65,14 +72,16 @@ def cross_correlate(source, destination, gaussian_width=7, divider=5, plot=False
         axes[0].imshow(pseudo_image_source, origin='lower', extent=bounds_source.flatten())
         bounds_destination = transfomation_destination.inverse(np.array([[0, 0], pseudo_image_destination.shape[::-1]])).T
         axes[1].imshow(pseudo_image_destination, origin='lower', extent=bounds_destination.flatten())
-
-        axes[2].imshow(correlation, origin='lower')
-        # plt.show()
+        bounds_correlation = np.array([[0, 0], np.array(correlation.shape[::-1])*divider]).T
+        bounds_correlation -= np.array([pseudo_image_source.shape[::-1]]).T*divider
+        axes[2].imshow(correlation_raw, origin='lower', extent=bounds_correlation.flatten())
+        if len(axes) > 3:
+            axes[3].imshow(correlation, origin='lower', extent=bounds_correlation.flatten())
 
     def correlation_coordinates_to_translation_coordinates(correlation_peak_coordinates):
         # return back_conversion_destination(correlation_peak_coordinates - np.array(pseudo_image_source.shape)[::-1])
-        transfomation_correlation = AffineTransform(translation=correlation_peak_coordinates - (np.array(pseudo_image_source.shape)[::-1]-1))
-        transfomation_destination_inverse = AffineTransform(transfomation_destination._inv_matrix)
-        return transfomation_source + transfomation_correlation + transfomation_destination_inverse
+        transformation_correlation = AffineTransform(translation=correlation_peak_coordinates - (np.array(pseudo_image_source.shape)[::-1]-1))
+        transformation_destination_inverse = AffineTransform(transfomation_destination._inv_matrix)
+        return transfomation_source + transformation_correlation + transformation_destination_inverse
 
     return correlation, correlation_coordinates_to_translation_coordinates
