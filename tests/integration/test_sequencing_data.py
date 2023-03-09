9635d97446ead72774e31d69715098e7ce14f050
Ivo Severins <i.w.h.severins@tudelft.nl>
2023-03-09 15:15:08 +0100

    Add test_sequencing_data_coordinates

--- tests/integration/test_sequencing_data.py
+++ tests/integration/test_sequencing_data.py
@@ -1,18 +1,31 @@
 import pytest
-from trace_analysis.plugins.sequencing.sequencing_data import parse_sam, fastq_generator, add_sequence_data_to_dataset
+from trace_analysis.plugins.sequencing.sequencing_data import parse_sam, fastq_generator, add_sequence_data_to_dataset, SequencingData
 
 
 @pytest.fixture
 def read1_fastq_filepath(shared_datadir):
     return shared_datadir / 'sequencing' / 'read1.fastq'
 
+
 @pytest.fixture
 def index1_fastq_filepath(shared_datadir):
     return shared_datadir / 'sequencing' / 'index1.fastq'
 
+
 @pytest.fixture
 def sam_filepath(shared_datadir):
     return shared_datadir / 'sequencing' / 'aligned.sam'
+
+
+@pytest.fixture
+def nc_filepath(sam_filepath):
+    test_parse_sam(sam_filepath)
+    return sam_filepath.with_suffix('.nc')
+
+
+@pytest.fixture
+def sequencing_data(nc_filepath):
+    return SequencingData(file_path=nc_filepath)
 
 
 def test_parse_sam(sam_filepath):
@@ -22,13 +35,16 @@ def test_parse_sam(sam_filepath):
 
 
 def test_fastq_generator(read1_fastq_filepath):
-    fqg = fastq_generator(read1_fastq_filepath)
+    fqg = fastq_generator(read1_fastq_filepath, 100)
     next(fqg)
 
-def test_add_sequence_data_to_dataset(sam_filepath, index1_fastq_filepath):
-    test_parse_sam(sam_filepath)
-    nc_filepath = sam_filepath.with_suffix('.nc')
+
+def test_add_sequence_data_to_dataset(nc_filepath, index1_fastq_filepath):
     add_sequence_data_to_dataset(nc_filepath, index1_fastq_filepath, 'index1')
+
+
+def test_sequencing_data_coordinates(sequencing_data):
+    sequencing_data.coordinates
 
 
 # def test_unlim_vs_lim_dim_netcdf4():
