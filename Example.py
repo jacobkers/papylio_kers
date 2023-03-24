39dea34e454ccf2f5de74c27751c2a8f00521a6f
Ivo Severins <i.w.h.severins@tudelft.nl>
2023-03-24 09:51:51 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,80 +1,44 @@
-import os
-import numpy as np
-from trace_analysis import Experiment # Import before matplotlib
+import trace_analysis as ta
 
-from matplotlib import pyplot as plt
-from sys import platform
-
-# if platform == "darwin":
-#     from matplotlib import use
-#     use('WXAgg')
-
-# from trace_analysis import InteractivePlot
-from trace_analysis.image_adapt.autoconfig import autoconfig_AND_perform_mapping, autoconfig
-
-
-# Define path to data, replace by your own directory
-# mainPath = r'D:\pathToDataFolder'
-mainPath = None # If mainPath is None, a window will appear to select a directory
-
-# Initialize an experiment
-exp = Experiment(mainPath)
-
-# Print files in experiment
+# Experiment import
+exp = ta.Experiment()
 exp.print_files()
 
-# Perform mapping
-perform_new_mapping = False
-if perform_new_mapping:
-    mapping_file_index = int(input('\nEnter file number for mapping... '))
-    mapping_file = exp.files[mapping_file_index]
 
-    # autoconfig_AND_perform_mapping(mapping_file_index, mainPath)
+# Emission channel mapping
+mapping_file_index = 0
+mapping_file = exp.files[mapping_file_index]
 
-    mapping_file.perform_mapping()
-    figure_mapping = plt.figure(101)
-    mapping_file.show_image(figure=figure_mapping)
-    mapping_file.mapping.show_mapping_transformation(figure=figure_mapping, show_source=True)
-    plt.show(block=False)
-    plt.pause(0.1)
+mapping_file.perform_mapping()
 
-# Run for specific files
-file_indices_of_interest = range(2, 110)
-
-selected_files = [file for file_index, file in enumerate(exp.files) if file_index in file_indices_of_interest]
-
-for file in selected_files:
-    # autoconfig(exp, file, opt='find_coordinates')
-
-    # Do not run for the mapping file
-    if file.is_mapping_file:
-        continue
-
-    # Do not run if there are already molecules loaded, i.e. if pks or traces files are present
-    # if file.molecules:
-    #     continue
-
-    print(f'\nWorking on {file}')
-    file.find_coordinates()
-    file.extract_traces()
+figure, axis = mapping_file.show_image()
+mapping_file.mapping.show(axis=axis, show_source=True)
 
 
-# show movie
-for file in selected_files:
-    figure_handle_movie = plt.figure()
-    # file.show_image(figure=figure_handle_movie)
-    file.show_image(figure=figure_handle_movie, vmin=120, vmax=200)
-    if file.is_mapping_file:
-        file.mapping.show_mapping_transformation(figure=figure_handle_movie)
-    else:
-        if file.coordinates.size > 0:
-            file.show_coordinates(figure=figure_handle_movie)
-plt.show()
-plt.pause(0.1)
+# File selection
+file_selection = exp.files[2:10]  # Selection of files: change to the desired file range
 
-# Show interactive plot
-file_index = 3
-molecules = exp.files[0].molecules
-int_plot = InteractivePlot(molecules)
-int_plot.plot_initialize()
-int_plot.plot_molecule()
+
+# Background subtraction
+file_selection.movie.determine_spatial_background_correction()
+
+
+# Molecule localization - test on one file
+test_file = file_selection[0]  # Change number to the desired file number
+test_file.find_coordinates()
+test_file.show_coordinates_in_image()
+
+
+# Molecule localization - apply to all files
+file_selection.find_coordinates()
+
+
+# Trace extraction
+file_selection.extract_traces()
+
+
+# Show traces
+test_file = file_selection[0]  # Change number to the desired file number
+test_file.show_traces(plot_variables=['intensity', 'FRET'],
+                     ylims=[(0, 35000), (0, 1)],
+                     colours=[('green', 'red'), ('blue')])
