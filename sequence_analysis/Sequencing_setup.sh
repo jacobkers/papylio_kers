1529991fe0f06d05209b704309a4448b6fa944d0
Ivo Severins <i.w.h.severins@tudelft.nl>
2023-04-08 17:13:50 +0200

    Update Sequencing_setup.sh

--- sequence_analysis/Sequencing_setup.sh
+++ sequence_analysis/Sequencing_setup.sh
@@ -3,4 +3,5 @@ pwd
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3.sh
 bash ~/miniconda3.sh -b -p $HOME/miniconda3
 rm ~/miniconda3.sh
-~/miniconda3/condabin/conda env create -f sequencing_environment.yml
+source ~/miniconda3/etc/profile.d/conda.sh
+conda env create -f sequencing_environment.yml
