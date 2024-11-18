dd2f4fad0e524f738e5e69084131980603bc306f
Ivo Severins <i.w.h.severins@tudelft.nl>
2024-11-18 15:49:10 +0900

    Delete sequence analysis folder

--- sequence_analysis/Analysis.cmd
+++
@@ -1,2 +0,0 @@
-ubuntu run ./Analysis.sh
-pause
--- sequence_analysis/Sequencing_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pwd
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3.sh
-bash ~/miniconda3.sh -b -p $HOME/miniconda3
-rm ~/miniconda3.sh
-source ~/miniconda3/etc/profile.d/conda.sh
-conda env create -f sequencing_environment.yml
