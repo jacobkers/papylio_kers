c9eb9d453fea72d8b8e1d05d8fc60916955415c9
Ivo Severins <52487180+ivoseverins@users.noreply.github.com>
2025-01-17 16:42:44 +0900

    Delete Update N drive code.cmd

--- Update N drive code.cmd
+++
@@ -1,5 +0,0 @@
-call conda activate trace_analysis
-rmdir "N:\tnw\BN\Shared\Ivo\traceAnalysis" /s
-mkdir "N:\tnw\BN\Shared\Ivo\traceAnalysis"
-git clone https://gitlab.tudelft.nl/ivoseverins/traceanalysis.git "N:\tnw\BN\Shared\Ivo\traceAnalysis" --branch develop
-pause
