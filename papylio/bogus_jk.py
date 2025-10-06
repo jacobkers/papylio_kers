c84087e5ad59c4994626e8c8c18f632d9e445b4f
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2025-10-06 14:00:22 +0200

    Revert "fork test"
    This reverts commit 5ecfe85ed180e11f7b942ea53289de79125d6f85.

--- papylio/bogus_jk.py
+++
@@ -1,6 +0,0 @@
-def bgs():
-    """"
-    just to check if my notebooks (in a separate repo)
-    find the forked repo-JK
-    """
-    print('this confirms code from papylio_kers was changed yet again and again and again')
