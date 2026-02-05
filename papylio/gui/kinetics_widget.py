83ddada705510db97180e10aed6e05fd26738a14
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2026-02-05 11:02:25 +0100

    Update kinetics_widget.py

--- papylio/gui/kinetics_widget.py
+++ papylio/gui/kinetics_widget.py
@@ -51,7 +51,7 @@ class KineticsWidget(QWidget):
 
         #main
         dwell_action_button = QPushButton('Get Dwells')
-        dwell_action_button.clicked.connect(self.perform_mapping)
+        dwell_action_button.clicked.connect(self.get_kinetics)
         dwell_help_button = QPushButton('Help!')
         dwell_help_button.clicked.connect(self.show_dwell_help)
 
