e09c76b8a7fa9e3aeaba029b9d4cebad7dd0aa8d
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2026-05-19 17:19:09 +0200

    1D and 2D histograms work

--- papylio/gui/histogram_widget.py
+++ papylio/gui/histogram_widget.py
@@ -19,28 +19,16 @@ class HistogramWidget(QWidget):
         super().__init__(parent)
         self.parent = parent
 
-        self.fig_histogram = Figure(figsize=(14, 3))
-        self.histogram_canvas = HistogramCanvas(self.fig_histogram)
+        self.fig_histogram_1D = Figure(figsize=(14, 3))
+        self.histogram_canvas_1D = HistogramCanvas_1D(self.fig_histogram_1D)
 
-        # Create toolbar, passing canvas as first parameter, parent (self, the MainWindow) as second.
-        #histogram_layout = QVBoxLayout()
-        #histogram_layout.addWidget(self.histogram_canvas)
-
-        # main control buttons
-        histogram_controls = build_control_layouts(
-            [make_push_button('Export', self.export_histogram, "export plot contents"),
-             ])
-
-        # tune panel on the right
-
-        histogram_panel_layout = QVBoxLayout()
-        histogram_panel_layout.setAlignment(Qt.AlignRight)
-        histogram_panel_layout.addWidget(histogram_controls)
+        self.fig_histogram_2D = Figure(figsize=(14, 3))
+        self.histogram_canvas_2D = HistogramCanvas_2D(self.fig_histogram_2D)
 
         # tab layout
         histogram_tab_layout = QHBoxLayout()
-        histogram_tab_layout.addWidget(self.histogram_canvas)
-        #histogram_tab_layout.addLayout(histogram_panel_layout)
+        histogram_tab_layout.addWidget(self.histogram_canvas_1D)
+        histogram_tab_layout.addWidget(self.histogram_canvas_2D)
 
         # other
         other_graph_layout = QHBoxLayout()
@@ -75,21 +63,19 @@ class HistogramWidget(QWidget):
 
     @file.setter
     def file(self, file):
-        self.histogram_canvas.file = file
+        self.histogram_canvas_1D.file = file
+        self.histogram_canvas_2D.file = file
         if file is None:
             self.setDisabled(True)
         else:
             self.setDisabled(False)
 
-    def export_histogram(self):
-        fpath=self.parent.experiment.analysis_path / 'Histograms'
-        wysiwyg_export(self.fig_histogram, filepath=fpath,  filename="histogram_export", filetype="csv")
 
-class HistogramCanvas(FigureCanvas):
+class HistogramCanvas_1D(FigureCanvas):
     def __init__(self, parent=None, width=14, height=7, dpi=100):
-        self.fig_histogram= mpl.figure.Figure(figsize=(width, height), dpi=dpi,
+        self.fig_histogram_1D= mpl.figure.Figure(figsize=(width, height), dpi=dpi,
                                         constrained_layout=True)  # , figsize=(2, 2))
-        super().__init__(self.fig_histogram)
+        super().__init__(self.fig_histogram_1D)
         self.parent = parent
         self._file = None
 
@@ -108,23 +94,42 @@ class HistogramCanvas(FigureCanvas):
             self.draw()
 
     def refresh(self):
-        #self.figure.clf()
-        #TODO here the histogram should be built and shown
-        self.fig_histogram.clear()
-        axes = self.fig_histogram.subplots(1, 2, sharex=True)
-        #selected_files = self.parent.experiment.selectedFiles
-        #self.fig_histogram.clf()
-
-        #if selected_files:
-        # TODO: here, perform the histogram function (possibly both 1D and 2D)
-        #test_file = selected_files[0]
+        #1D-histogram
+        self.fig_histogram_1D.clear()
+        axis = self.fig_histogram_1D.subplots(1, 1, sharex=True)
         bins = [i * 0.01 for i in range(200)]
-        self.file.show_histogram(variable='FRET', axis=axes[0], bins=bins)
+        self.file.show_histogram(variable='FRET', axis=axis, bins=bins)
+        self.draw()
 
+class HistogramCanvas_2D(FigureCanvas):
+    def __init__(self, parent=None, width=14, height=7, dpi=100):
+        self.fig_histogram_2D = mpl.figure.Figure(figsize=(width, height), dpi=dpi,
+                                                  constrained_layout=True)  # , figsize=(2, 2))
+        super().__init__(self.fig_histogram_2D)
+        self.parent = parent
+        self._file = None
+
+    @property
+    def file(self):
+        return self._file
+
+    @file.setter
+    def file(self, file):
+        if file is not None and file is not self._file:
+            self._file = file
+            self.refresh()
+        elif file is None:
+            self._file = None
+            self.figure.clf()
+            self.draw()
+
+    def refresh(self):
+        # 1D-histogram
+        self.fig_histogram_2D.clear()
+        axis = self.fig_histogram_2D.subplots(1, 1, sharex=True)
         self.file.histogram_2D_intensity_per_channel(
-            ax=axes[1],
+            ax=axis,
             bins=50,
             show_marginal=False
         )
-
         self.draw()
